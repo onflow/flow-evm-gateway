b8b8ac9ace7927ac129deedc20dcb186cf0d6cc4
Ardit Marku <markoupetr@gmail.com>
2024-01-06 17:57:10 +0200

    Add TODO for including more options in Config

--- api/config.go
+++ api/config.go
@@ -6,6 +6,12 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 )
 
+// TODO(m-Peter) Add more config options, such as:
+// - host
+// - port
+// - access URL to connect the indexer to
+// - whether JSON-RPC is exposed HTTP/WebSocket or both
+// - some connection timeout options etc
 type Config struct {
 	ChainID  *big.Int
 	Coinbase common.Address
