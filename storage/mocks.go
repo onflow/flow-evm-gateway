d3cdbf724622e28d03ca8da086b774f3e6ecfad9
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2024-01-29 15:08:05 +0100

    add block mock

---
+++ storage/mocks.go
@@ -0,0 +1,22 @@
+package storage
+
+import (
+	"fmt"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/onflow/flow-go/fvm/evm/types"
+)
+
+func newBlock(height uint64) *types.Block {
+	parent := common.HexToHash(fmt.Sprintf("0x0%d", height-1))
+	if height == 0 {
+		parent = common.Hash{}
+	}
+
+	return &types.Block{
+		ParentBlockHash:   parent,
+		Height:            height,
+		TotalSupply:       1000,
+		ReceiptRoot:       common.HexToHash(fmt.Sprintf("0x1337%d", height)),
+		TransactionHashes: nil,
+	}
+}
