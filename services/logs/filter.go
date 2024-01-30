5b0816849e0d2afba9cf9ff2bce77267468e2b1e
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2024-01-30 17:12:08 +0100

    Revert "add filter match"
    This reverts commit b58ac1503c58130b7943976e9f74387aa6634495.

--- services/logs/filter.go
+++ services/logs/filter.go
@@ -4,21 +4,19 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	gethTypes "github.com/ethereum/go-ethereum/core/types"
 	"math/big"
-	"slices"
 )
 
 type Filter struct {
-	address common.Address
-	topics  []common.Hash
+	addresses []common.Address
+	topics    [][]common.Hash
 
-	// todo maybe move to get method
 	blockID    *common.Hash // Block hash if filtering a single block
 	start, end *big.Int     // Range interval if filtering multiple blocks
 
 	provider Provider
 }
 
-func (f *Filter) get() error {
+func (f *Filter) match() error {
 	bloom := gethTypes.Bloom{} // create bloom
 
 	logs, err := f.provider.Get(bloom, f.blockID, f.start, f.end)
@@ -29,27 +27,7 @@ func (f *Filter) get() error {
 	for {
 		select {
 		case l := <-logs:
-			if f.matches(l) {
 
-			}
 		}
 	}
-}
-
-func (f *Filter) matches(log *gethTypes.Log) bool {
-	if len(f.topics) > len(log.Topics) {
-		return false
-	}
-
-	for _, topic := range f.topics {
-		if !slices.Contains(log.Topics, topic) {
-			return false
-		}
-	}
-
-	if f.address.Cmp(log.Address) != 0 {
-		return false
-	}
-
-	return true
 }
