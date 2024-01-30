8ab4c83a825f48d100db353386ca0f55cd13d528
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2024-01-30 17:12:08 +0100

    Revert "update storage provider to enable block id search"
    This reverts commit da7eb8c09b8c9f2ea3fccf74d03d405d2e51bfc5.

--- services/logs/provider.go
+++ services/logs/provider.go
@@ -2,7 +2,6 @@ package logs
 
 import (
 	"bytes"
-	"github.com/ethereum/go-ethereum/common"
 	gethTypes "github.com/ethereum/go-ethereum/core/types"
 	"github.com/onflow/flow-evm-gateway/storage"
 	"math/big"
@@ -16,7 +15,7 @@ type Provider interface {
 	// Start and end have special values "latest", if both are set to "latest" that means we want
 	// to get all the upcoming new logs that match the bloom filter. If the start and end are
 	// defined as anything else we fetch already indexed logs.
-	Get(bloom gethTypes.Bloom, hash *common.Hash, start, end *big.Int) (chan []*gethTypes.Log, error)
+	Get(bloom gethTypes.Bloom, start, end *big.Int) (chan []*gethTypes.Log, error)
 }
 
 var _ Provider = &StorageProvider{}
@@ -24,42 +23,20 @@ var _ Provider = &StorageProvider{}
 // StorageProvider uses the indexer storage to fetch matching logs.
 type StorageProvider struct {
 	receipts storage.ReceiptIndexer
-	blocks   storage.BlockIndexer
 }
 
-func (s StorageProvider) Get(
-	bloom gethTypes.Bloom,
-	hash *common.Hash,
-	start, end *big.Int,
-) (chan []*gethTypes.Log, error) {
-	logs := make(chan []*gethTypes.Log, 0)
-	defer close(logs)
-
-	// if we need to search in a single block provided by ID
-	if hash != nil {
-		bl, err := s.blocks.GetByID(*hash)
-		if err != nil {
-			return nil, err
-		}
-		receipt, err := s.receipts.GetByBlockHeight(big.NewInt(int64(bl.Height)))
-		if err != nil {
-			return nil, err
-		}
-
-		if matchBloom(receipt.Bloom, bloom) {
-			logs <- receipt.Logs
-			return logs, nil
-		}
-	}
-
-	// if we are searching in block range defined by start and end
+func (s StorageProvider) Get(bloom gethTypes.Bloom, start, end *big.Int) (chan []*gethTypes.Log, error) {
 	rangeBlooms, err := s.receipts.BloomsForBlockRange(start, end)
 	if err != nil {
 		return nil, err
 	}
 
+	logs := make(chan []*gethTypes.Log, 0)
+	defer close(logs)
+
 	for height, b := range rangeBlooms {
-		if matchBloom(b, bloom) {
+		// todo add correct bloom matching using composed blooms
+		if bytes.Equal(b.Bytes(), bloom.Bytes()) {
 			receipt, err := s.receipts.GetByBlockHeight(height)
 			if err != nil {
 				return nil, err
@@ -77,12 +54,7 @@ var _ Provider = &StreamProvider{}
 // StreamProvider uses stream of logs as they come in to retrieve matching logs.
 type StreamProvider struct{}
 
-func (s StreamProvider) Get(bloom gethTypes.Bloom, hash *common.Hash, start, end *big.Int) (chan []*gethTypes.Log, error) {
+func (s StreamProvider) Get(bloom gethTypes.Bloom, start, end *big.Int) (chan []*gethTypes.Log, error) {
 	//TODO implement me
 	panic("implement me")
-}
-
-func matchBloom(match, with gethTypes.Bloom) bool {
-	// todo add correct bloom matching using composed blooms
-	return bytes.Equal(match.Bytes(), with.Bytes())
 }
