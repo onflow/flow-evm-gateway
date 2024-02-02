63a0a48b2de948122da51a10e4dceddbca0caf74
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2024-02-02 18:05:53 +0100

    add block ingestion test

--- services/events/ingestion_test.go
+++ services/events/ingestion_test.go
@@ -2,41 +2,86 @@ package events
 
 import (
 	"context"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/onflow/cadence"
 	"github.com/onflow/flow-evm-gateway/services/events/mocks"
 	storageMock "github.com/onflow/flow-evm-gateway/storage/mocks"
 	"github.com/onflow/flow-go-sdk"
 	"github.com/onflow/flow-go/fvm/evm/types"
 	"github.com/rs/zerolog"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
+	"math/big"
 	"testing"
 )
 
-func TestBlockIngestion(t *testing.T) {
+func newBlock(height uint64) (*types.Block, cadence.Event, *types.Event, error) {
+	block := &types.Block{
+		ParentBlockHash: common.HexToHash("0x1"),
+		Height:          height,
+		TotalSupply:     big.NewInt(100),
+		ReceiptRoot:     common.HexToHash("0x2"),
+		TransactionHashes: []common.Hash{
+			common.HexToHash("0xf1"),
+		},
+	}
+
+	blockEvent := types.NewBlockExecutedEvent(block)
+	blockCdc, err := blockEvent.Payload.CadenceEvent()
+
+	return block, blockCdc, blockEvent, err
+}
+
+func TestSerialBlockIngestion(t *testing.T) {
 	receipts := &storageMock.ReceiptIndexer{}
 	transactions := &storageMock.TransactionIndexer{}
 
 	latestHeight := uint64(10)
 
 	blocks := &storageMock.BlockIndexer{}
-	blocks.On("LatestHeight").Return(func() (uint64, error) {
-		return latestHeight, nil
-	})
-	blocks.On("Store").Return(func(block *types.Block) error {
-		// todo assert correct block
-		return nil
-	})
+	blocks.
+		On("LatestHeight").
+		Return(func() (uint64, error) {
+			return latestHeight, nil
+		}).
+		Once() // make sure this isn't called multiple times
 
-	eventsChan := make(<-chan flow.BlockEvents)
+	eventsChan := make(chan flow.BlockEvents)
 	subscriber := &mocks.Subscriber{}
 	subscriber.
-		On("Subscribe").
+		On("Subscribe", mock.AnythingOfType("context.backgroundCtx"), mock.AnythingOfType("uint64")).
 		Return(func(ctx context.Context, latest uint64) (<-chan flow.BlockEvents, <-chan error, error) {
 			return eventsChan, make(<-chan error), nil
 		})
 
 	engine := NewEventIngestionEngine(subscriber, blocks, receipts, transactions, zerolog.Nop())
 
-	err := engine.Start(context.Background())
-	require.NoError(t, err)
+	go func() {
+		err := engine.Start(context.Background())
+		require.NoError(t, err)
+	}()
 
+	for i := latestHeight + 1; i < latestHeight+20; i++ {
+		block, blockCdc, blockEvent, err := newBlock(i)
+		require.NoError(t, err)
+
+		blocks.
+			On("Store", mock.AnythingOfType("*types.Block")).
+			Return(func(storeBlock *types.Block) error {
+				assert.Equal(t, block, storeBlock)
+				return nil
+			}).
+			Once()
+
+		eventsChan <- flow.BlockEvents{
+			Events: []flow.Event{{
+				Type:  string(blockEvent.Etype),
+				Value: blockCdc,
+			}},
+		}
+	}
+
+	close(eventsChan)
+	// todo <-engine.Done()
 }
