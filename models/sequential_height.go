1f2c2d6e7d920127eb0accdf1a5db628f070ddc6
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2024-02-05 15:21:05 +0100

    add sequential height tracker

---
+++ models/sequential_height.go
@@ -0,0 +1,36 @@
+package models
+
+import (
+	"errors"
+	"sync/atomic"
+)
+
+var InvalidHeightErr = errors.New("invalid height")
+
+// SequentialHeight tracks a block height and enforces rules about
+// the valid next height.
+type SequentialHeight struct {
+	height atomic.Uint64
+}
+
+// Increment the height value according to the rules.
+// A valid next height must be either incremented
+// by one, or must be the same as previous height to make the action idempotent.
+// Expected errors:
+// if the height is not incremented according to the rules a InvalidHeightErr error is returned
+func (s *SequentialHeight) Increment(nextHeight uint64) error {
+	for {
+		current := s.height.Load()
+		if nextHeight-current > 1 {
+			return InvalidHeightErr
+		}
+
+		if s.height.CompareAndSwap(current, nextHeight) {
+			return nil
+		}
+	}
+}
+
+func (s *SequentialHeight) Load() uint64 {
+	return s.height.Load()
+}
