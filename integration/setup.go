b14c966aa5a95e8f37b90676cb89edce470679fb
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2024-02-06 14:42:21 +0100

    fix loggers

--- integration/setup.go
+++ integration/setup.go
@@ -3,19 +3,20 @@ package integration
 import (
 	"context"
 	"fmt"
-	"github.com/onflow/flow-evm-gateway/storage/memory"
-	"github.com/onflow/flow-go-sdk/access/grpc"
-
 	"github.com/onflow/cadence"
 	"github.com/onflow/flow-emulator/adapters"
 	"github.com/onflow/flow-emulator/emulator"
 	"github.com/onflow/flow-emulator/server"
 	"github.com/onflow/flow-evm-gateway/services/events"
+	"github.com/onflow/flow-evm-gateway/storage"
+	"github.com/onflow/flow-evm-gateway/storage/memory"
 	sdk "github.com/onflow/flow-go-sdk"
+	"github.com/onflow/flow-go-sdk/access/grpc"
 	"github.com/onflow/flow-go-sdk/crypto"
 	"github.com/onflow/flow-go/model/flow"
 	"github.com/rs/zerolog"
 	"os"
+	"time"
 )
 
 const testPrivateKey = "61ceacbdce419e25ee8e7c2beceee170a05c9cab1e725a955b15ba94dcd747d2"
@@ -28,41 +29,60 @@ func startEmulator() (*server.EmulatorServer, error) {
 		return nil, err
 	}
 
-	srv := server.NewEmulatorServer(&logger, &server.Config{
+	log := logger.With().Str("component", "emulator").Logger().Level(zerolog.DebugLevel)
+	srv := server.NewEmulatorServer(&log, &server.Config{
 		ServicePrivateKey:  pkey,
 		ServiceKeySigAlgo:  crypto.ECDSA_P256,
 		ServiceKeyHashAlgo: crypto.SHA3_256,
 		GenesisTokenSupply: 10000,
 		EVMEnabled:         true,
 		WithContracts:      true,
-		Host:               grpc.EmulatorHost,
+		Host:               "127.0.0.1",
 	})
 
-	return srv, srv.Listen()
+	go func() {
+		defer srv.Stop()
+		srv.Start()
+	}()
+
+	time.Sleep(200 * time.Millisecond) // give it some time to start, dummy but ok for test
+
+	return srv, nil
 }
 
-func startEventIngestionEngine(ctx context.Context) error {
-	client, err := grpc.NewClient(grpc.EmulatorHost, nil)
+// startEventIngestionEngine will start up the event engine with the grpc subscriber
+// listening for events.
+// todo for now we return index storage as a way to check the data if it was correctly
+// indexed this will be in future replaced with evm gateway API access
+func startEventIngestionEngine(ctx context.Context) (storage.BlockIndexer, storage.ReceiptIndexer, storage.TransactionIndexer, error) {
+	client, err := grpc.NewClient("127.0.0.1:3569")
 	if err != nil {
-		return err
+		return nil, nil, nil, err
+	}
+
+	blk, err := client.GetLatestBlock(ctx, false)
+	if err != nil {
+		return nil, nil, nil, err
 	}
 
 	subscriber := events.NewRPCSubscriber(client)
-	blocks := memory.NewBlockStorage()
+	blocks := memory.NewBlockStorage(memory.WithLatestHeight(blk.Height))
 	receipts := memory.NewReceiptStorage()
 	txs := memory.NewTransactionStorage()
 
-	engine := events.NewEventIngestionEngine(subscriber, blocks, receipts, txs, logger)
+	log := logger.With().Str("component", "ingestion").Logger()
+	engine := events.NewEventIngestionEngine(subscriber, blocks, receipts, txs, log)
 
 	go func() {
 		err = engine.Start(ctx)
 		if err != nil {
+			logger.Error().Err(err)
 			panic(err)
 		}
 	}()
 
 	<-engine.Ready() // wait for engine to be ready
-	return nil
+	return blocks, receipts, txs, err
 }
 
 // sendTransaction sends an evm transaction to the emulator, the code provided doesn't
@@ -78,7 +98,8 @@ func sendTransaction(emu emulator.Emulator, code string, args ...cadence.Value) 
 		code,
 	))
 
-	adapter := adapters.NewSDKAdapter(&logger, emu)
+	log := logger.With().Str("component", "adapter").Logger().Level(zerolog.InfoLevel)
+	adapter := adapters.NewSDKAdapter(&log, emu)
 
 	tx := sdk.NewTransaction().
 		SetScript(codeWrapper).
