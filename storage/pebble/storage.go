3b7f8a1394ed160d3f39f7c8d731c1ab93e9b767
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2024-02-12 15:03:27 +0100

    add get set helpers

--- storage/pebble/storage.go
+++ storage/pebble/storage.go
@@ -1,19 +1,23 @@
 package pebble
 
 import (
+	"errors"
 	"fmt"
 	"github.com/cockroachdb/pebble"
 	"github.com/ethereum/go-ethereum/rlp"
+	errs "github.com/onflow/flow-evm-gateway/storage/errors"
 	"github.com/onflow/flow-go/fvm/evm/types"
+	"github.com/rs/zerolog"
 	"io"
 )
 
 type Storage struct {
-	db *pebble.DB
+	db  *pebble.DB
+	log zerolog.Logger
 }
 
 // New creates a new storage instance using the provided dir location as the storage directory.
-func New(dir string) (*Storage, error) {
+func New(dir string, log zerolog.Logger) (*Storage, error) {
 	cache := pebble.NewCache(1 << 20)
 	defer cache.Unref()
 
@@ -57,18 +61,46 @@ func New(dir string) (*Storage, error) {
 		return nil, fmt.Errorf("failed to open db: %w", err)
 	}
 
-	return &Storage{db: db}, nil
+	return &Storage{db: db, log: log}, nil
+}
+
+func (s *Storage) set(keyCode byte, key any, value []byte) error {
+	// by default, we disable sync since write operations are idempotent and since crash is not expected to be common
+	// we can rely on idempotency to resolve such a crash and gain performance benefits of having sync off
+	writeOpts := &pebble.WriteOptions{Sync: false}
+
+	prefixedKey := makePrefix(keyCode, key)
+	return s.db.Set(prefixedKey, value, writeOpts)
+}
+
+func (s *Storage) get(keyCode byte, key any) ([]byte, error) {
+	prefixedKey := makePrefix(keyCode, key)
+
+	data, closer, err := s.db.Get(prefixedKey)
+	if err != nil {
+		if errors.Is(err, pebble.ErrNotFound) {
+			return nil, errs.NotFound
+		}
+		return nil, err
+	}
+
+	defer func(closer io.Closer) {
+		err = closer.Close()
+		if err != nil {
+			s.log.Error().Err(err)
+		}
+	}(closer)
+
+	return data, nil
 }
 
 func (s *Storage) storeBlock(block *types.Block) error {
-	key := []byte(fmt.Sprintf("blocks/%d", block.Height))
 	val, err := block.ToBytes()
 	if err != nil {
 		return err
 	}
 
-	// we make the sync false since we can always repeat the indexing in case of crash which should be rare so the performance optimization is worth it
-	return s.db.Set(key, val, &pebble.WriteOptions{Sync: false})
+	return s.set(blockHeightKey, block.Height, val)
 }
 
 func (s *Storage) getBlock(height uint64) (*types.Block, error) {
