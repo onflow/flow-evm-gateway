4c3515986617652820d05fdb2741d9ec01fa51fc
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2024-02-12 14:49:48 +0100

    add retrieve block test

--- storage/pebble/storage_test.go
+++ storage/pebble/storage_test.go
@@ -2,6 +2,7 @@ package pebble
 
 import (
 	"github.com/onflow/flow-evm-gateway/storage/mocks"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"os"
 	"testing"
@@ -11,9 +12,20 @@ func TestBlock(t *testing.T) {
 
 	runDB("store block", t, func(t *testing.T, db *Storage) {
 		bl := mocks.NewBlock(10)
+		err := db.storeBlock(bl)
+		require.NoError(t, err)
+	})
+
+	runDB("store and retrieve", t, func(t *testing.T, db *Storage) {
+		const height = uint64(12)
+		bl := mocks.NewBlock(height)
 
 		err := db.storeBlock(bl)
 		require.NoError(t, err)
+
+		block, err := db.getBlock(height)
+		require.NoError(t, err)
+		assert.Equal(t, bl, block)
 	})
 }
 
