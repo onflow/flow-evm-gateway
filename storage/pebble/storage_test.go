3f0809355448d79cc23eb88577f70c333a27c099
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2024-02-12 13:41:56 +0100

    add basic store test

--- storage/pebble/storage_test.go
+++ storage/pebble/storage_test.go
@@ -3,16 +3,31 @@ package pebble
 import (
 	"github.com/onflow/flow-evm-gateway/storage/mocks"
 	"github.com/stretchr/testify/require"
+	"os"
 	"testing"
 )
 
 func TestBlock(t *testing.T) {
 
-	t.Run("store block", func(t *testing.T) {
-		db, err := New()
-		require.NoError(t, err)
+	runDB("store block", t, func(t *testing.T, db *Storage) {
+		bl := mocks.NewBlock(10)
 
-		block := mocks.NewBlock(10)
-		db.storeBlock()
+		err := db.storeBlock(bl)
+		require.NoError(t, err)
+	})
+}
+
+func runDB(name string, t *testing.T, f func(t *testing.T, db *Storage)) {
+	dir, err := os.MkdirTemp("", "flow-testing-temp-")
+	require.NoError(t, err)
+	defer func() {
+		require.NoError(t, os.RemoveAll(dir))
+	}()
+
+	db, err := New(dir)
+	require.NoError(t, err)
+
+	t.Run(name, func(t *testing.T) {
+		f(t, db)
 	})
 }
