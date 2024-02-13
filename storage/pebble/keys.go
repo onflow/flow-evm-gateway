67e4083f55397507432d934ba3cc6e2b62d7595e
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2024-02-13 14:52:14 +0100

    add blooms for heights

--- storage/pebble/keys.go
+++ storage/pebble/keys.go
@@ -20,6 +20,7 @@ const (
 	firstHeightKey  = byte(101)
 )
 
+// makePrefix makes a key used internally to store the values
 func makePrefix(code byte, key ...[]byte) []byte {
 	prefix := make([]byte, 1)
 	prefix[0] = code
@@ -35,6 +36,12 @@ func makePrefix(code byte, key ...[]byte) []byte {
 	return append(prefix, key[0]...)
 }
 
+// stripPrefix from the key and return only the key value
+func stripPrefix(key []byte) []byte {
+	return key[1:]
+}
+
+// uint64Bytes converts the uint64 value to bytes
 func uint64Bytes(height uint64) []byte {
 	b := make([]byte, 8)
 	binary.BigEndian.PutUint64(b, height)
--- storage/pebble/receipts.go
+++ storage/pebble/receipts.go
@@ -1,9 +1,13 @@
 package pebble
 
 import (
+	"encoding/binary"
+	"fmt"
+	"github.com/cockroachdb/pebble"
 	"github.com/ethereum/go-ethereum/common"
 	gethTypes "github.com/ethereum/go-ethereum/core/types"
 	"github.com/onflow/flow-evm-gateway/storage"
+	"github.com/onflow/flow-evm-gateway/storage/errors"
 	"math/big"
 	"sync"
 )
@@ -13,6 +17,7 @@ var _ storage.ReceiptIndexer = &Receipts{}
 type Receipts struct {
 	store *Storage
 	mux   sync.RWMutex
+	first uint64
 }
 
 func NewReceipts(store *Storage) *Receipts {
@@ -58,7 +63,91 @@ func (r *Receipts) GetByBlockHeight(height *big.Int) (*gethTypes.Receipt, error)
 }
 
 func (r *Receipts) BloomsForBlockRange(start, end *big.Int) ([]gethTypes.Bloom, []*big.Int, error) {
-	panic("TODO")
+	if start.Cmp(end) > 0 {
+		return nil, nil, fmt.Errorf("start is bigger than end: %w", errors.InvalidRange)
+	}
+
+	// make sure the first and last height are within indexed values
+	first, last, err := r.getFirstLast()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	if start.Uint64() < first || start.Uint64() > last {
+		return nil, nil, fmt.Errorf(
+			"start value %d is not within the indexed range of [%d - %d]: %w",
+			start,
+			first,
+			last,
+			errors.InvalidRange,
+		)
+	}
+
+	if end.Uint64() < first || end.Uint64() > last {
+		return nil, nil, fmt.Errorf(
+			"end value %d is not within the indexed range of [%d - %d]: %w",
+			start,
+			first,
+			last,
+			errors.InvalidRange,
+		)
+	}
+
+	// we increase end by 1 since the range is exclusive at the upper boundary: [start, end)
+	endInclusive := new(big.Int).Add(end, big.NewInt(1))
+	iterator, err := r.store.db.NewIter(&pebble.IterOptions{
+		LowerBound: makePrefix(bloomHeightKey, start.Bytes()),        // inclusive
+		UpperBound: makePrefix(bloomHeightKey, endInclusive.Bytes()), // exclusive
+	})
+	if err != nil {
+		return nil, nil, err
+	}
+	defer func() {
+		err := iterator.Close()
+		if err != nil {
+			r.store.log.Error().Err(err).Msg("failed to close receipt iterator")
+		}
+	}()
+
+	caps := end.Div(end, start).Uint64() // max capacity for slices
+	blooms := make([]gethTypes.Bloom, 0, caps)
+	heights := make([]*big.Int, 0, caps)
+
+	for iterator.First(); iterator.Valid(); iterator.Next() {
+		val, err := iterator.ValueAndErr()
+		if err != nil {
+			return nil, nil, err
+		}
+
+		bloom := gethTypes.BytesToBloom(val)
+		h := stripPrefix(iterator.Key())
+		height := binary.BigEndian.Uint64(h)
+
+		blooms = append(blooms, bloom)
+		heights = append(heights, big.NewInt(int64(height)))
+	}
+
+	return blooms, heights, nil
+}
+
+func (r *Receipts) getFirstLast() (uint64, uint64, error) {
+	l, err := r.store.get(latestHeightKey)
+	if err != nil {
+		return 0, 0, err
+	}
+	last := binary.BigEndian.Uint64(l)
+
+	if r.first != 0 {
+		return r.first, last, nil
+	}
+
+	first, err := r.store.get(firstHeightKey)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	r.first = binary.BigEndian.Uint64(first)
+	return r.first, last, nil
 }
 
 func (r *Receipts) getReceipt(keyCode byte, key []byte) (*gethTypes.Receipt, error) {
