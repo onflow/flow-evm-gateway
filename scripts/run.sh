b00ff69bc96de88203f4ec20b6a22d30b871d3f2
Alex Ni <12097569+nialexsan@users.noreply.github.com>
2024-02-21 11:22:53 -0500

    conditionally run emulator for crescendo

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
+if [ "$FLOW_NETWORK" = "crescendo" ]; then
+  # Start the first process & redirect output to a temporary file
+  ./flow-x86_64-linux- emulator --evm-enabled > temp_output.txt &
 
-./evm-gateway --network=$FLOW_NETWORK
+  # PID of the first process
+  FIRST_PROCESS_PID=$!
+
+  # Monitor the temporary file for a specific output
+  PATTERN="3569"
+  while ! grep -q "$PATTERN" temp_output.txt; do
+    sleep 1
+  done
+
+  # Once the pattern is found, you can kill the first process if needed
+  # kill $FIRST_PROCESS_PID
+
+  # Run the second process
+
+  ./flow-x86_64-linux- transactions send /flow-evm-gateway/create_bridged_account.cdc 1500.0 --network=emulator --signer=emulator-account && ./evm-gateway
+
+  # Clean up temporary file
+  rm temp_output.txt
+else 
+  ./evm-gateway --network=$FLOW_NETWORK
+fi
