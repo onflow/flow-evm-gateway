e90b8411890f39214829f32356c0d257dbc8f4a8
Alex Ni <12097569+nialexsan@users.noreply.github.com>
2024-02-21 10:14:02 -0500

    fix envs

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./flow-x86_64-linux- transactions send /flow-evm-gateway/create_bridged_account.cdc 1500.0 --network=$FLOW_NETWORK --signer=emulator-account && ./evm-gateway
+./evm-gateway --network=$FLOW_NETWORK
