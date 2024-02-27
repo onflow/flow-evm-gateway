d56863dd3f66df24836415559475756b261760b6
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2024-02-27 22:03:47 +0100

    update run and multi-keys

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,7 +2,7 @@
   --init-cadence-height 6479 \
   --flow-network-id previewnet \
   --coinbase FACF71692421039876a5BB4F10EF7A439D8ef61E \
-  --coa-address 0x4e0a9367e445c6f9 \
-  --coa-key-file ./previewnet-keys.json
+  --coa-address 0x585ef6547d41cc98 \
+  --coa-key-file ./previewnet-keys.json \
   --coa-resource-create \
   --gas-price 0
