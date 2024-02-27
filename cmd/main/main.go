f98b4c084609b828339ae00c2bf2328823c40056
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2024-02-27 16:52:33 +0100

    use main with signals

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"github.com/onflow/flow-evm-gateway/bootstrap"
 	"github.com/onflow/flow-evm-gateway/config"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
@@ -11,8 +16,17 @@ func main() {
 		panic(err)
 	}
 
-	err = bootstrap.Start(cfg)
+	ctx, cancel := context.WithCancel(context.Background())
+
+	err = bootstrap.Start(ctx, cfg)
 	if err != nil {
 		panic(err)
 	}
+
+	osSig := make(chan os.Signal, 1)
+	signal.Notify(osSig, syscall.SIGINT, syscall.SIGTERM)
+
+	<-osSig
+	fmt.Println("OS Signal to shutdown received, shutting down")
+	cancel()
 }
