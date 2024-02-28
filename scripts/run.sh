2756b2d98d9ecd021106fe555f3366ba785d3706
Jerome P <frankly.watson@gmail.com>
2024-02-27 16:32:34 -0800

    Set RPC port to 3000

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,6 +5,7 @@
 
   ./evm-gateway --access-node-grpc-host access-001.previewnet1.nodes.onflow.org:9000 \
     --init-cadence-height 6479 \
+    --rpc-port 3000 \
     --flow-network-id previewnet \
     --coinbase FACF71692421039876a5BB4F10EF7A439D8ef61E \
     --coa-address 0xaa6d2abaf5bfc626 \
