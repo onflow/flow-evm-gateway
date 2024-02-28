4c92a80bd4799809232b7e0896efa43fe3896ceb
Jerome P <frankly.watson@gmail.com>
2024-02-27 16:15:44 -0800

    Strip out conditional to test without noise of missing vars

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-if [ "$FLOW_NETWORK" = "testnet" ] || [ "$FLOW_NETWORK" = "mainnet" ] || [ "$FLOW_NETWORK" = "canarynet" ] || [ "$FLOW_NETWORK" = "previewnet" ]; then
-  ./evm-gateway --flow-network-id=$FLOW_NETWORK
-else
+## if [ "$FLOW_NETWORK" = "testnet" ] || [ "$FLOW_NETWORK" = "mainnet" ] || [ "$FLOW_NETWORK" = "canarynet" ] || [ "$FLOW_NETWORK" = "previewnet" ]; then
+##  ./evm-gateway --flow-network-id=$FLOW_NETWORK
+## else
+
   ./evm-gateway --access-node-grpc-host access-001.previewnet1.nodes.onflow.org:9000 \
     --init-cadence-height 6479 \
     --flow-network-id previewnet \
@@ -10,4 +11,4 @@ else
     --coa-key 555b8ccf5dd26d4fa575221791305481d3b23e47e3e1883001eba36d6b5e65ae \
     --coa-resource-create \
     --gas-price 0
-fi
+##fi
