3566387f8fe4fe62cd5c99150290c208b4a61edf
Gregor G <75445744+sideninja@users.noreply.github.com>
2024-02-28 18:51:25 +0100

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,5 @@
 ./evm-gateway --access-node-grpc-host access.previewnet.nodes.onflow.org:9000 \
-  --init-cadence-height 6479 \
+  --init-cadence-height 0 \
   --rpc-port 3000 \
   --flow-network-id previewnet \
   --coinbase FACF71692421039876a5BB4F10EF7A439D8ef61E \
