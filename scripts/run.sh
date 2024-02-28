1297089cecee432129b9b86a2fbf27a185e877c8
Jerome P <frankly.watson@gmail.com>
2024-02-27 17:03:24 -0800

    Fix run.sh to specify port Fix deploy.yml to reflect previewnet change and

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,6 @@
-./evm-gateway --access-node-grpc-host access-001.previewnet1.nodes.onflow.org:9000 \
+./evm-gateway --access-node-grpc-host access.previewnet.nodes.onflow.org:9000 \
   --init-cadence-height 6479 \
+  --rpc-port 3000 \
   --flow-network-id previewnet \
   --coinbase FACF71692421039876a5BB4F10EF7A439D8ef61E \
   --coa-address 0x585ef6547d41cc98 \
