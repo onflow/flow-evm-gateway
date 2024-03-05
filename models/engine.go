f89a457644d01f6d41e7e5553ca7469736439e95
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2024-03-05 18:19:05 +0100

    recover after timeout

--- models/engine.go
+++ models/engine.go
@@ -1,6 +1,11 @@
 package models
 
-import "context"
+import (
+	"context"
+	"fmt"
+	"github.com/rs/zerolog"
+	"time"
+)
 
 // Engine defines a processing unit
 type Engine interface {
@@ -16,13 +21,34 @@ type Engine interface {
 
 var _ Engine = &RestartableEngine{}
 
+func NewRestartableEngine(engine Engine, retries int, logger zerolog.Logger) *RestartableEngine {
+	// build a fibonachi sequence, we could support more strategies in future
+	// we use 0 as first run shouldn't be delayed
+	backoff := []time.Duration{0, time.Second, time.Second}
+	for i := len(backoff); i < retries; i++ {
+		backoff = append(backoff, backoff[i-2]+backoff[i-1])
+	}
+
+	logger = logger.With().Str("component", "restartable-engine").Logger()
+
+	return &RestartableEngine{
+		engine:  engine,
+		backoff: backoff,
+		logger:  logger,
+	}
+}
+
 // RestartableEngine is an engine wrapper that tries to restart
 // the engine in case of starting errors.
 //
-// The strategy of the restarts contains simple backoff time and
+// The strategy of the restarts contains fibonachi backoff time and
 // limited number of retries that can be configured.
+// Here are backoff values for different retries provided:
+// 1s 1s 2s 3s 5s 8s 13s 21s 34s 55s 1m29s 2m24s 3m53s 6m17s 10m10s 16m27s 26m37s 43m4s 1h9m41s
 type RestartableEngine struct {
-	engine Engine
+	logger  zerolog.Logger
+	engine  Engine
+	backoff []time.Duration
 }
 
 func (r *RestartableEngine) Stop() {
@@ -38,8 +64,30 @@ func (r *RestartableEngine) Ready() <-chan struct{} {
 }
 
 func (r *RestartableEngine) Run(ctx context.Context) error {
-	// todo add restart logic
-	return r.engine.Run(ctx)
+	var err error
+	for i, b := range r.backoff {
+		<-time.After(b) // wait for it
+		if b > 0 {
+			r.logger.Warn().Msg("restarting the engine now")
+		}
+
+		err = r.engine.Run(ctx)
+		if err == nil {
+			// don't restart if no error is returned, normal after stop procedure is done
+			return nil
+		}
+		if !IsRecoverableError(err) {
+			r.logger.Error().Err(err).Msg("received unrecoverable error")
+			// if error is not recoverable just die
+			return err
+		}
+
+		r.logger.Error().Err(err).Msg(fmt.Sprintf("received recoverable error, restarting for the %d time after backoff time", i))
+	}
+
+	r.logger.Error().Msg("failed to recover and restart the engine, stop retrying")
+	// if after retries we still get an error it's time to stop
+	return err
 }
 
 type EngineStatus struct {
