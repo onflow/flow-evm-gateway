739a35e428a80b09259ab6e88b664aedd6ac0dba
Ardit Marku <markoupetr@gmail.com>
2024-03-08 15:36:55 +0200

    Update models/geth_tx.go
    Co-authored-by: Gregor G. <75445744+sideninja@users.noreply.github.com>

--- models/geth_tx.go
+++ models/geth_tx.go
@@ -7,60 +7,19 @@ import (
 	gethTypes "github.com/ethereum/go-ethereum/core/types"
 )
 
+
+var _ FlowEVMTxData = &GethTx{}
+
 type GethTx struct {
-	Tx *gethTypes.Transaction
+	*gethTypes.Transaction
 }
 
-func (gt GethTx) Hash() common.Hash {
-	return gt.Tx.Hash()
+func (gt *GethTx) From() (common.Address, error) {
+    return gethTypes.Sender(gethTypes.LatestSignerForChainID(gt.ChainId()), gt.Transaction)
 }
 
-func (gt GethTx) RawSignatureValues() (v *big.Int, r *big.Int, s *big.Int) {
-	return gt.Tx.RawSignatureValues()
+func (gt *GethTx) MarshalBinary() ([]byte, error) {
+	encoded, err := gt.MarshalBinary()
+	return append([]byte{gt.Type()}, encoded...), err
 }
 
-func (gt GethTx) From() common.Address {
-	from, _ := gethTypes.Sender(gethTypes.LatestSignerForChainID(gt.Tx.ChainId()), gt.Tx)
-	return from
-}
-
-func (gt GethTx) To() *common.Address {
-	return gt.Tx.To()
-}
-
-func (gt GethTx) Data() []byte {
-	return gt.Tx.Data()
-}
-
-func (gt GethTx) Nonce() uint64 {
-	return gt.Tx.Nonce()
-}
-
-func (gt GethTx) Value() *big.Int {
-	return gt.Tx.Value()
-}
-
-func (gt GethTx) Type() uint8 {
-	return gt.Tx.Type()
-}
-
-func (gt GethTx) Gas() uint64 {
-	return gt.Tx.Gas()
-}
-
-func (gt GethTx) GasPrice() *big.Int {
-	return gt.Tx.GasPrice()
-}
-
-func (gt GethTx) BlobGas() uint64 {
-	return gt.Tx.BlobGas()
-}
-
-func (gt GethTx) Size() uint64 {
-	return gt.Tx.Size()
-}
-
-func (gt GethTx) MarshalBinary() ([]byte, error) {
-	encoded, err := gt.Tx.MarshalBinary()
-	return append([]byte{gt.Tx.Type()}, encoded...), err
-}
