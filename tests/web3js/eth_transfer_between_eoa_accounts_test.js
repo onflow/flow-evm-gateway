0a2c00d04fda6eb534fd7bb2abb65d1a0eaa3480
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2024-04-05 19:43:57 +0200

    send eoa test finished

--- tests/web3js/eth_transfer_between_eoa_accounts_test.js
+++ tests/web3js/eth_transfer_between_eoa_accounts_test.js
@@ -13,25 +13,29 @@ it('transfer flow between two EOA accounts', async() => {
 
     // get sender balance
     let senderBalance = await web3.eth.getBalance(conf.eoa.address)
-    assert.equal(senderBalance, conf.fundedAmount)
+    assert.equal(senderBalance, utils.toWei(conf.fundedAmount, "ether"))
 
     let transferValue = utils.toWei("0.5", "ether")
     let transfer = await conf.eoa.signTransaction({
         from: conf.eoa.address,
+        to: receiver.address,
         value: transferValue,
         gasPrice: '0',
-        gasLimit: 50000,
+        gasLimit: 55000,
     })
 
     // make sure receipt is correct
     let receipt = await web3.eth.sendSignedTransaction(transfer.rawTransaction)
     assert.equal(receipt.status, conf.successStatus)
-    assert.equal(receipt.from, conf.eoa.address)
-    assert.equal(receipt.to, receiver.address)
+    assert.equal(receipt.from, conf.eoa.address.toLowerCase()) // todo checksum
+    assert.equal(receipt.to, receiver.address.toLowerCase()) // todo checksum
 
     // check balance was moved
     receiverWei = await web3.eth.getBalance(receiver.address)
     assert.equal(receiverWei, transferValue)
+
+    senderBalance = await web3.eth.getBalance(conf.eoa.address)
+    assert.equal(senderBalance, utils.toWei(conf.fundedAmount, "ether")-transferValue)
 
     // make sure latest block includes the transfer tx
     let latest = await web3.eth.getBlockNumber()
