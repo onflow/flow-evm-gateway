2f563708bf2a18aa9db4b3731fb482abbe8f1b74
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2024-04-05 19:45:29 +0200

    value check

--- tests/web3js/eth_transfer_between_eoa_accounts_test.js
+++ tests/web3js/eth_transfer_between_eoa_accounts_test.js
@@ -41,4 +41,5 @@ it('transfer flow between two EOA accounts', async() => {
     let latest = await web3.eth.getBlockNumber()
     let transferTx = await web3.eth.getTransactionFromBlock(latest, 0)
     assert.equal(transferTx.hash, transfer.transactionHash)
+    assert.equal(transferTx.value, transferValue)
 })
