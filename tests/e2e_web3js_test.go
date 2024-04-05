dd4109e5bc36f2a8f020f68dc7b1c4a2789d27f2
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2024-04-05 17:19:00 +0200

    add new test

--- tests/e2e_web3js_test.go
+++ tests/e2e_web3js_test.go
@@ -4,6 +4,10 @@ import (
 	"testing"
 )
 
-func Test_Web3Acceptance(t *testing.T) {
+func Test_NonInteractiveTest(t *testing.T) {
 	runWeb3Test(t, "eth_non-interactive_test")
+}
+
+func Test_DeployContract(t *testing.T) {
+	runWeb3Test(t, "eth_deploy_test")
 }
