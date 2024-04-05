8d2c9e2095c1a1d358d296be8952b1eedbcd4892
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2024-04-05 18:25:12 +0200

    rename

--- tests/e2e_web3js_test.go
+++ tests/e2e_web3js_test.go
@@ -9,5 +9,5 @@ func Test_NonInteractiveTest(t *testing.T) {
 }
 
 func Test_DeployContract(t *testing.T) {
-	runWeb3Test(t, "eth_deploy_test")
+	runWeb3Test(t, "eth_deploy_contract_and_interact_test")
 }
