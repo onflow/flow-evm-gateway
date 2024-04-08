88084d7a164566c65d956e2895118e3ddf74b87b
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2024-04-08 16:15:29 +0200

    remove ws

--- tests/web3js/config.js
+++ tests/web3js/config.js
@@ -4,7 +4,6 @@ const web3 = new Web3("http://localhost:8545")
 
 module.exports = {
     web3: web3,
-    web3WS: new Web3("ws://localhost:8545"),
     eoa: web3.eth.accounts.privateKeyToAccount("0xf6d5333177711e562cabf1f311916196ee6ffc2a07966d9d4628094073bd5442"),
     fundedAmount: 5.0,
     startBlockHeight: 3n, // start block height after setup accounts
