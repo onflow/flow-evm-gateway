fcc883eaad25a142d5152167cae36e6ad785bab7
Gregor Gololicic <gregor.gololicic@dapperlabs.com>
2024-04-09 15:16:40 +0200

    test ordering

--- models/events_test.go
+++ models/events_test.go
@@ -13,62 +13,79 @@ import (
 
 func TestCadenceEvents_Block(t *testing.T) {
 
-	evmBlock := types.NewBlock(common.HexToHash("0x01"), 1, big.NewInt(100), common.HexToHash("0x02"), nil)
-	ev := types.NewBlockExecutedEvent(evmBlock)
-	cadenceEvent, err := ev.Payload.CadenceEvent()
-	require.NoError(t, err)
-
 	v, _ := cadence.NewString("invalid")
 	invalid, _ := cadence.NewValue(v)
+
+	b0, e0, err := newBlock(0)
+	require.NoError(t, err)
+
+	b1, e1, err := newBlock(1)
+	require.NoError(t, err)
+
+	b2, e2, err := newBlock(2)
+	require.NoError(t, err)
 
 	tests := []struct {
 		name   string
 		events flow.BlockEvents
-		block  *types.Block
+		blocks []*types.Block
 		err    error
 	}{
 		{
-			name: "BlockExecutedEventExists",
-			events: flow.BlockEvents{Events: []flow.Event{
-				{
-					Type:             string(ev.Etype),
-					TransactionIndex: 0,
-					EventIndex:       0,
-					Value:            cadenceEvent,
-				},
-			}},
-			block: evmBlock,
+			name:   "BlockExecutedEventExists",
+			events: flow.BlockEvents{Events: []flow.Event{e0}},
+			blocks: []*types.Block{b0},
 		}, {
 			name:   "BlockExecutedEventEmpty",
 			events: flow.BlockEvents{Events: []flow.Event{}},
-			block:  nil,
+			blocks: []*types.Block{},
 		}, {
 			name: "BlockExecutedNotFound",
 			events: flow.BlockEvents{Events: []flow.Event{{
-				Type:             string(ev.Etype),
-				TransactionIndex: 0,
-				EventIndex:       0,
-				Value:            cadence.NewEvent([]cadence.Value{invalid}),
+				Type:  e0.Type,
+				Value: cadence.NewEvent([]cadence.Value{invalid}),
 			}}},
-			block: nil,
+			blocks: []*types.Block{},
+		}, {
+			name:   "BlockExecutedOutOfOrder",
+			events: flow.BlockEvents{Events: []flow.Event{e0, e2, e1}},
+			blocks: []*types.Block{b0, b1, b2},
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			c := NewCadenceEvents(tt.events)
-			b, err := c.Block()
+			blocks, err := c.Blocks()
 			require.NoError(t, err)
 
-			if tt.block != nil {
-				ttHash, err := tt.block.Hash()
-				require.NoError(t, err)
-				hash, err := b.Hash()
-				require.NoError(t, err)
-				assert.Equal(t, ttHash, hash)
+			if tt.blocks != nil {
+				for i, ttb := range tt.blocks {
+					ttHash, err := ttb.Hash()
+					require.NoError(t, err)
+					hash, err := blocks[i].Hash()
+					require.NoError(t, err)
+					assert.Equal(t, ttHash, hash)
+				}
 			} else {
-				assert.Nil(t, b)
+				assert.Nil(t, blocks)
 			}
 		})
 	}
+}
+
+func newBlock(height uint64) (*types.Block, flow.Event, error) {
+	evmBlock := types.NewBlock(common.HexToHash("0x01"), height, big.NewInt(100), common.HexToHash("0x02"), nil)
+	ev := types.NewBlockExecutedEvent(evmBlock)
+	cadenceEvent, err := ev.Payload.CadenceEvent()
+	if err != nil {
+		return nil, flow.Event{}, err
+	}
+
+	flowEvent := flow.Event{
+		Type:  string(ev.Etype),
+		Value: cadenceEvent,
+	}
+
+	return evmBlock, flowEvent, nil
 }
