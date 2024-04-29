9fdcab99ec613d07b6a01e3d4eac7334ae2a4b94
Gregor G <gr3go@pm.me>
2024-04-29 16:07:06 +0200

    change decoder and fix tests

--- models/block.go
+++ models/block.go
@@ -2,40 +2,33 @@ package models
 
 import (
 	"fmt"
+	"math/big"
 
 	"github.com/onflow/cadence"
 	"github.com/onflow/flow-go/fvm/evm/types"
 	"github.com/onflow/go-ethereum/common"
 )
 
-type blockEventPayload struct {
-	Height            uint64           `cadence:"height"`
-	Hash              string           `cadence:"hash"`
-	TotalSupply       cadence.Int      `cadence:"totalSupply"`
-	ParentBlockHash   string           `cadence:"parentHash"`
-	ReceiptRoot       string           `cadence:"receiptRoot"`
-	TransactionHashes []cadence.String `cadence:"transactionHashes"`
-}
-
 // decodeBlock takes a cadence event that contains executed block payload and
 // decodes it into the Block type.
 func decodeBlock(event cadence.Event) (*types.Block, error) {
-	var b blockEventPayload
-	err := cadence.DecodeFields(event, &b)
+	payload, err := types.DecodeBlockEventPayload(event)
 	if err != nil {
 		return nil, fmt.Errorf("failed to cadence decode block: %w", err)
 	}
 
-	hashes := make([]common.Hash, len(b.TransactionHashes))
-	for i, h := range b.TransactionHashes {
+	hashes := make([]common.Hash, len(payload.TransactionHashes))
+	for i, h := range payload.TransactionHashes {
 		hashes[i] = common.HexToHash(h.ToGoValue().(string))
 	}
 
 	return &types.Block{
-		ParentBlockHash:   common.HexToHash(b.ParentBlockHash),
-		Height:            b.Height,
-		TotalSupply:       b.TotalSupply.Value,
-		ReceiptRoot:       common.HexToHash(b.ReceiptRoot),
+		ParentBlockHash:   common.HexToHash(payload.ParentBlockHash),
+		Height:            payload.Height,
+		Timestamp:         payload.Timestamp,
+		TotalSupply:       big.NewInt(int64(payload.TotalSupply.Int())),
+		ReceiptRoot:       common.HexToHash(payload.ReceiptRoot),
 		TransactionHashes: hashes,
+		TotalGasUsed:      payload.TotalGasUsed,
 	}, nil
 }
