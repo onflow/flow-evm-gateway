575240f6056bb1e2d82750134ac9c55b68dfb91f
sideninja <75445744+sideninja@users.noreply.github.com>
2024-05-13 17:14:53 +0200

    add more custom tests

--- tests/web3js/eth_streaming_filters_test.js
+++ tests/web3js/eth_streaming_filters_test.js
@@ -6,48 +6,61 @@ const storageABI = require('../fixtures/storageABI.json')
 
 const timeout = 20
 
-async function assertFilterLogs(contract, filterObj, expectedLogs) {
-    const subscription = await contract.events.Calculated(filterObj)
-
-    let subId = new Promise((res => subscription.on("connected", res)))
-
-    subscription.on("error", err => console.log(err))
-
+async function assertFilterLogs(subscription, expectedLogs) {
     let allLogs = []
-    let logs = new Promise((res, rej) => subscription.on("data", data => {
-        allLogs.push(data)
-        console.log("# event received: ", data.returnValues)
+    return new Promise((res, rej) => {
+        subscription.on("error", err => {
+            console.log(err)
+            rej(err)
+        })
 
-        // we do this timeout as a trick, to wait if we receive more logs than we should
-        // since resolving at the expected length right away might miss another
-        // log that would unexpectedly come after.
-        setTimeout(() => {
-            if (allLogs.length === expectedLogs.length) {
-                subscription.unsubscribe()
+        subscription.on("data", data => {
+            allLogs.push(data)
 
-                // after we receive all logs, we make sure each received
-                // logs matches the entry in the expected log by all the values
-                for (let i = 0; i < expectedLogs.length; i++) {
-                    let expected = expectedLogs[i]
+            // we do this timeout as a trick, to wait if we receive more logs than we should
+            // since resolving at the expected length right away might miss another
+            // log that would unexpectedly come after.
+            setTimeout(() => {
+                if (allLogs.length === expectedLogs.length) {
+                    subscription.unsubscribe()
 
-                    for (const key in expected) {
-                        let expectedVal = expected[key]
-                        let actualVal = allLogs[i].returnValues[key]
-                        assert.equal(actualVal, expectedVal)
+                    // after we receive all logs, we make sure each received
+                    // logs matches the entry in the expected log by all the values
+                    for (let i = 0; i < expectedLogs.length; i++) {
+                        let expected = expectedLogs[i]
+
+                        // if we have ABI decoded event values as return values
+                        if (allLogs[i].returnValues != undefined) {
+                            for (const key in expected) {
+                                let expectedVal = expected[key]
+                                assert.isDefined(allLogs[i].returnValues)
+                                let actualVal = allLogs[i].returnValues[key]
+                                assert.equal(actualVal, expectedVal)
+                            }
+                        } else { // otherwise compare by position
+                            let position = 2 // we start at 2 since first two topics are address and event name
+                            for (const key in expected) {
+                                let expectedVal = expected[key]
+                                assert.isDefined(allLogs[i].topics)
+                                // convert big int hex values
+                                let actualVal = BigInt(allLogs[i].topics[position])
+                                if (actualVal & (1n << 255n)) {
+                                    actualVal -= (1n << 256n) // convert as signed int256 number
+                                }
+
+                                assert.equal(actualVal, expectedVal)
+                                position++
+                            }
+                        }
                     }
+
+                    res(allLogs)
+                } else if (allLogs.length > expectedLogs.length) {
+                    rej(allLogs)
                 }
-
-                res(allLogs)
-            } else if (allLogs.length > expectedLogs.length) {
-                rej(allLogs)
-            }
-        }, 500)
-    }))
-
-    let id = await subId
-    assert.isString(id)
-
-    return logs
+            }, 500)
+        })
+    })
 }
 
 // todo add streaming of logs without address provided (any address)
@@ -56,6 +69,10 @@ async function assertFilterLogs(contract, filterObj, expectedLogs) {
 it('streaming of logs using filters', async() => {
     let contractDeployment = await helpers.deployContract("storage")
     let contractAddress = contractDeployment.receipt.contractAddress
+
+    // we deploy another contract to use for filtering by address
+    let contractDeployment2 = await helpers.deployContract("storage")
+    let contractAddress2 = contractDeployment2.receipt.contractAddress
 
     let repeatA = 10
     const testValues = [
@@ -67,46 +84,56 @@ it('streaming of logs using filters', async() => {
     ]
 
     let ws = new Web3("ws://127.0.0.1:8545")
-    let storageContract = new ws.eth.Contract(storageABI, contractAddress);
+    let storageContract = new ws.eth.Contract(storageABI, contractAddress)
+    let storageContract2 = new ws.eth.Contract(storageABI, contractAddress);
+
+    let calculatedEvent = storageContract.events.Calculated
+    let calculatedEvent2 = storageContract.events.Calculated
+
+    let rawSubscribe = filter => ws.eth.subscribe('logs', filter)
+
 
     // wait for subscription for a bit
     await new Promise((res, rej) => setTimeout(() => res(), 500))
 
-    let firstBlock = await ws.eth.getBlockNumber()
-
     let allTests = [
         // stream all events
-        assertFilterLogs(storageContract, {}, testValues),
-        // stream only one event that has numA set to -1
-        assertFilterLogs(
-            storageContract,
-            { filter: {numA: -1} },
-            testValues.filter(v => v.numA === -1)
-        ),
-        // stream only events that have numB set to 200
-        assertFilterLogs(
-            storageContract,
-            { filter: {numB: 200} },
-            testValues.filter(v => v.numB === 200)
-        ),
-        // stream events that have numA set to 10 and numB set to 200
-        assertFilterLogs(
-            storageContract,
-            { filter: {numA: repeatA, numB: 200} },
-            testValues.filter(v => v.numB === 200 && v.numA === repeatA)
-        ),
-        // stream only events that have numA value set to 10
-        assertFilterLogs(
-            storageContract,
-            { filter: {numA: repeatA} },
-            testValues.filter(v => v.numA === repeatA)
-        ),
+        // assertFilterLogs(calculatedEvent({ }), testValues),
+        // // stream only one event that has numA set to -1
+        // assertFilterLogs(
+        //     calculatedEvent({ filter: {numA: -1} }),
+        //     testValues.filter(v => v.numA === -1)
+        // ),
+        // // stream only events that have numB set to 200
+        // assertFilterLogs(
+        //     calculatedEvent({ filter: {numB: 200} }),
+        //     testValues.filter(v => v.numB === 200)
+        // ),
+        // // stream events that have numA set to 10 and numB set to 200
+        // assertFilterLogs(
+        //     calculatedEvent({ filter: {numA: repeatA, numB: 200} }),
+        //     testValues.filter(v => v.numB === 200 && v.numA === repeatA)
+        // ),
+        // // stream only events that have numA value set to 10
+        // assertFilterLogs(
+        //     calculatedEvent({ filter: {numA: repeatA} }),
+        //     testValues.filter(v => v.numA === repeatA)
+        // ),
         // stream events that have numB 200 OR 300 value
-        assertFilterLogs(
-            storageContract,
-            { filter: {numB: [200, 300]} },
-            testValues.filter(v => v.numB === 200 || v.numB === 300)
-        ),
+        // assertFilterLogs(
+        //     calculatedEvent({ filter: {numB: [200, 300]} }),
+        //     testValues.filter(v => v.numB === 200 || v.numB === 300)
+        // ),
+
+        // we also test the raw subscriptions since they allow for specifying raw values
+
+        // stream all events by any contract, we have two same contracts, so we duplicate expected values and in order
+        //assertFilterLogs(await rawSubscribe({}), testValues.concat(testValues)),
+
+        // return all values by only a single contract
+        //assertFilterLogs(await rawSubscribe({ address: contractAddress }), testValues)
+
+        assertFilterLogs(await rawSubscribe({ address: contractAddress, fromBlock: "0x0" }), testValues)
     ]
 
 
@@ -125,7 +152,19 @@ it('streaming of logs using filters', async() => {
         assert.equal(res.receipt.status, conf.successStatus)
     }
 
+    for (const { numA, numB } of testValues) {
+        let res = await helpers.signAndSend({
+            from: conf.eoa.address,
+            to: contractAddress2,
+            data: storageContract2.methods.sum(numA, numB).encodeABI(),
+            gas: 1000000,
+            gasPrice: 0
+        })
+        assert.equal(res.receipt.status, conf.successStatus)
+    }
+
     await Promise.all(allTests)
+
     await ws.eth.clearSubscriptions()
 
     process.exit(0)
