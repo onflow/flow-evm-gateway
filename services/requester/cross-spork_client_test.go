05d9c5602721c2624e556dfe7b68bb30540a311b
sideninja <75445744+sideninja@users.noreply.github.com>
2024-05-23 15:15:06 +0200

    add client test

--- services/requester/cross-spork_client_test.go
+++ services/requester/cross-spork_client_test.go
@@ -1,8 +1,12 @@
 package requester
 
 import (
+	"context"
 	"testing"
 
+	"github.com/onflow/flow-go-sdk"
+	"github.com/onflow/flow-go-sdk/access"
+	"github.com/rs/zerolog"
 	"github.com/stretchr/testify/require"
 
 	"github.com/onflow/flow-evm-gateway/services/testutils"
@@ -60,5 +64,64 @@ func Test_CrossSporkClients(t *testing.T) {
 		require.NoError(t, clients.add(client2))
 
 		require.False(t, clients.continuous())
+	})
+}
+
+func Test_CrossSpork(t *testing.T) {
+	t.Run("client", func(t *testing.T) {
+		past1Last := uint64(300)
+		past2Last := uint64(500)
+		currentLast := uint64(1000)
+		current := testutils.SetupClientForRange(501, currentLast)
+		past1 := testutils.SetupClientForRange(100, past1Last)
+		past2 := testutils.SetupClientForRange(301, past2Last)
+
+		client, err := NewCrossSporkClient(current, []access.Client{past2, past1}, zerolog.Nop())
+		require.NoError(t, err)
+
+		c, err := client.getClientForHeight(150)
+		require.NoError(t, err)
+		require.Equal(t, past1, c)
+
+		c, err = client.getClientForHeight(past2Last - 1)
+		require.NoError(t, err)
+		require.Equal(t, past2, c)
+
+		c, err = client.getClientForHeight(600)
+		require.NoError(t, err)
+		require.Equal(t, current, c)
+
+		c, err = client.getClientForHeight(10)
+		require.Nil(t, c)
+		require.ErrorIs(t, err, ErrOutOfRange)
+
+		require.True(t, client.IsPastSpork(200))
+		require.True(t, client.IsPastSpork(past1Last))
+		require.False(t, client.IsPastSpork(past2Last+1))
+		require.False(t, client.IsPastSpork(600))
+
+		_, err = client.ExecuteScriptAtBlockHeight(context.Background(), 20, []byte{}, nil)
+		require.ErrorIs(t, err, ErrOutOfRange)
+
+		_, err = client.GetBlockHeaderByHeight(context.Background(), 20)
+		require.ErrorIs(t, err, ErrOutOfRange)
+
+		_, _, err = client.SubscribeEventsByBlockHeight(context.Background(), 20, flow.EventFilter{}, nil)
+		require.ErrorIs(t, err, ErrOutOfRange)
+
+		height, err := client.GetLatestHeightForSpork(context.Background(), past2Last-10)
+		require.NoError(t, err)
+		require.Equal(t, past2Last, height)
+
+		height, err = client.GetLatestHeightForSpork(context.Background(), past1Last-10)
+		require.NoError(t, err)
+		require.Equal(t, past1Last, height)
+
+		height, err = client.GetLatestHeightForSpork(context.Background(), currentLast-10)
+		require.NoError(t, err)
+		require.Equal(t, currentLast, height)
+
+		_, err = client.GetLatestHeightForSpork(context.Background(), 10)
+		require.ErrorIs(t, err, ErrOutOfRange)
 	})
 }
