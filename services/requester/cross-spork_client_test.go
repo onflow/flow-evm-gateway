a5810fc8da95f5e6df167b6111a4323d2379cbb0
sideninja <75445744+sideninja@users.noreply.github.com>
2024-05-23 14:56:17 +0200

    add cross spork client tests

---
+++ services/requester/cross-spork_client_test.go
@@ -0,0 +1,55 @@
+package requester
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/onflow/flow-evm-gateway/services/testutils"
+)
+
+func Test_CrossSporkClient(t *testing.T) {
+	t.Run("contains", func(t *testing.T) {
+		first := uint64(10)
+		last := uint64(100)
+		client := sporkClient{
+			firstHeight: first,
+			lastHeight:  last,
+			client:      nil,
+		}
+
+		require.True(t, client.contains(first+1))
+		require.True(t, client.contains(last-1))
+		require.True(t, client.contains(first))
+		require.True(t, client.contains(last))
+		require.False(t, client.contains(2))
+		require.False(t, client.contains(200))
+	})
+}
+
+func Test_CrossSporkClients(t *testing.T) {
+	t.Run("add and validate", func(t *testing.T) {
+		clients := &sporkClients{}
+
+		client1 := testutils.SetupClientForRange(10, 100)
+		client2 := testutils.SetupClientForRange(101, 200)
+
+		err := clients.add(client1)
+		require.NoError(t, err)
+
+		err = clients.add(client2)
+		require.NoError(t, err)
+
+		require.True(t, clients.continuous())
+
+		require.Equal(t, client1, clients.get(10))
+		require.Equal(t, client1, clients.get(100))
+		require.Equal(t, client1, clients.get(20))
+		require.Equal(t, client2, clients.get(120))
+		require.Equal(t, client2, clients.get(101))
+		require.Equal(t, client2, clients.get(200))
+
+		require.Equal(t, nil, clients.get(5))
+		require.Equal(t, nil, clients.get(300))
+	})
+}
