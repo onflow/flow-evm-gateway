96c186c867ddf05914e36f3406f6502477ee73da
sideninja <75445744+sideninja@users.noreply.github.com>
2024-05-30 14:48:41 +0200

    fix timeouts

--- tests/web3js/eth_rate_limit_test.js
+++ tests/web3js/eth_rate_limit_test.js
@@ -1,17 +1,21 @@
-const conf = require('./config')
 const { assert } = require('chai')
 const {Web3} = require("web3")
 
-it('rate limit after X requests', async () => {
+it('rate limit after X requests', async function () {
+    this.timeout(0)
     setTimeout(() => process.exit(0), 5000) // make sure the process exits
     let ws = new Web3("ws://127.0.0.1:8545")
 
-    let requestLimit = 50 // this should be synced with the value on server config
+    // wait for ws connection to establish and reset rate-limit timer
+    await new Promise(res => setTimeout(res, 1500))
+
+    // this should be synced with the value on server config
+    let requestLimit = 50
     let requestsMade = 0
     let requestsFailed = 0
     let requests = 60
 
-    for (let i = 0; i <= requests; i++) {
+    for (let i = 0; i < requests; i++) {
         try {
             await ws.eth.getBlockNumber()
             requestsMade++
@@ -25,7 +29,7 @@ it('rate limit after X requests', async () => {
 
     await new Promise(res => setTimeout(res, 1000))
 
-    // after 1 second we can repeat
+    // after 1 second which is the rate-limit interval we can repeat
     requestsMade = 0
     requestsFailed = 0
 
