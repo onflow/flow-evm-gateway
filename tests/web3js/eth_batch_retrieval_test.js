608059ef76d35914065565351f98ea2e98751bb2
Ardit Marku <markoupetr@gmail.com>
2024-05-30 14:33:12 +0300

    Update tests for batch transactions

--- tests/web3js/eth_batch_retrieval_test.js
+++ tests/web3js/eth_batch_retrieval_test.js
@@ -2,7 +2,6 @@ const { assert } = require('chai')
 const conf = require('./config')
 const web3 = conf.web3
 
-
 it('retrieve batch transactions', async () => {
   // this test relies on the setup of batched transactions found in ../e2e_web3js_test.go
 
@@ -13,13 +12,11 @@ it('retrieve batch transactions', async () => {
   assert.lengthOf(block.transactions, batchSize)
 
   for (let i = 0; i < block.transactions.length; i++) {
-    let tx = await web3.eth.getTransactionFromBlock(latestHeight, block.number)
-    console.log(tx)
-    console.log("-----")
+    let tx = await web3.eth.getTransactionFromBlock(latestHeight, i)
     assert.equal(tx.blockNumber, block.number, "wrong block number")
     assert.equal(tx.blockHash, block.hash, "wrong block hash")
     assert.equal(tx.type, 0, "wrong type")
-    //assert.equal(tx.transactionIndex, i, "wrong index")
+    assert.equal(tx.transactionIndex, i, "wrong index")
     assert.isBelow(i, batchSize, "wrong batch size")
   }
 })
