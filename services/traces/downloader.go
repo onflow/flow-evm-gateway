9e9896529f8f8dca62d2ea1f971f4437719d0c97
sideninja <75445744+sideninja@users.noreply.github.com>
2024-06-04 16:13:13 +0200

    use hash type

--- services/traces/downloader.go
+++ services/traces/downloader.go
@@ -8,6 +8,7 @@ import (
 	"time"
 
 	"cloud.google.com/go/storage"
+	"github.com/onflow/go-ethereum/common"
 	"github.com/rs/zerolog"
 )
 
@@ -15,7 +16,7 @@ const downloadTimeout = 60 * time.Second
 
 type Downloader interface {
 	// Download traces or returning an error with the failure
-	Download(id string) (json.RawMessage, error)
+	Download(id common.Hash) (json.RawMessage, error)
 }
 
 var _ Downloader = &GCPDownloader{}
@@ -50,14 +51,14 @@ func NewGCPDownloader(bucketName string, logger zerolog.Logger) (*GCPDownloader,
 	}, nil
 }
 
-func (g *GCPDownloader) Download(id string) (json.RawMessage, error) {
-	l := g.logger.With().Str("tx-id", id).Logger()
+func (g *GCPDownloader) Download(id common.Hash) (json.RawMessage, error) {
+	l := g.logger.With().Str("tx-id", id.String()).Logger()
 	l.Debug().Msg("downloading transaction trace")
 
 	ctx, cancel := context.WithTimeout(context.Background(), downloadTimeout)
 	defer cancel()
 
-	rc, err := g.client.Bucket(g.bucketName).Object(id).NewReader(ctx)
+	rc, err := g.client.Bucket(g.bucketName).Object(id.String()).NewReader(ctx)
 	if err != nil {
 		return nil, fmt.Errorf("failed to download id %s: %w", id, err)
 	}
