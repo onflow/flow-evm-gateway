b1f02a3fcf42ee843ad95b5d3062d1298dbe6507
sideninja <75445744+sideninja@users.noreply.github.com>
2024-06-05 14:51:33 +0200

    improve downloader with retries

--- services/traces/downloader.go
+++ services/traces/downloader.go
@@ -8,23 +8,24 @@ import (
 	"time"
 
 	"cloud.google.com/go/storage"
+	"github.com/onflow/flow-go-sdk"
 	"github.com/onflow/go-ethereum/common"
 	"github.com/rs/zerolog"
 )
 
-const downloadTimeout = 60 * time.Second
+const downloadTimeout = 5 * time.Minute
 
 type Downloader interface {
 	// Download traces or returning an error with the failure
-	Download(id common.Hash) (json.RawMessage, error)
+	Download(txID common.Hash, blockIO flow.Identifier) (json.RawMessage, error)
 }
 
 var _ Downloader = &GCPDownloader{}
 
 type GCPDownloader struct {
-	client     *storage.Client
-	logger     zerolog.Logger
-	bucketName string
+	client *storage.Client
+	logger zerolog.Logger
+	bucket *storage.BucketHandle
 }
 
 func NewGCPDownloader(bucketName string, logger zerolog.Logger) (*GCPDownloader, error) {
@@ -38,27 +39,33 @@ func NewGCPDownloader(bucketName string, logger zerolog.Logger) (*GCPDownloader,
 		return nil, fmt.Errorf("storage.NewClient: %w", err)
 	}
 
+	bucket := client.Bucket(bucketName)
 	// try accessing buckets to validate settings
-	_, err = client.Bucket(bucketName).Attrs(ctx)
-	if err != nil {
+	if _, err = bucket.Attrs(ctx); err != nil {
 		return nil, fmt.Errorf("error accessing bucket: %s, make sure bucket exists: %w", bucketName, err)
 	}
 
 	return &GCPDownloader{
-		client:     client,
-		logger:     logger,
-		bucketName: bucketName,
+		client: client,
+		logger: logger,
+		bucket: bucket,
 	}, nil
 }
 
-func (g *GCPDownloader) Download(id common.Hash) (json.RawMessage, error) {
-	l := g.logger.With().Str("tx-id", id.String()).Logger()
+func (g *GCPDownloader) Download(txID common.Hash, blockID flow.Identifier) (json.RawMessage, error) {
+	l := g.logger.With().
+		Str("tx-id", txID.String()).
+		Str("cadence-block-id", blockID.String()).
+		Logger()
+
 	l.Debug().Msg("downloading transaction trace")
 
 	ctx, cancel := context.WithTimeout(context.Background(), downloadTimeout)
 	defer cancel()
 
-	rc, err := g.client.Bucket(g.bucketName).Object(id.String()).NewReader(ctx)
+	id := fmt.Sprintf("%s-%s", blockID.String(), txID.String())
+
+	rc, err := g.bucket.Object(id).NewReader(ctx)
 	if err != nil {
 		return nil, fmt.Errorf("failed to download id %s: %w", id, err)
 	}
