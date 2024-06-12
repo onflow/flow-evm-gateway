126e3ec257eb33eb74be3d3578d6f4c7feb58fb6
sideninja <75445744+sideninja@users.noreply.github.com>
2024-06-12 15:05:08 +0200

    implement adding to pool tx for result

--- services/requester/pool.go
+++ services/requester/pool.go
@@ -2,36 +2,54 @@ package requester
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	"github.com/onflow/flow-go-sdk"
+	gethCommon "github.com/onflow/go-ethereum/common"
+	gethTypes "github.com/onflow/go-ethereum/core/types"
 )
+
+// todo this is a simple implementation of the transaction pool that is mostly used
+// to track the status of submitted transaction, but transactions will always be submitted
+// right away, future improvements can make it so the transactions are collected in the pool
+// and after submitted based on different strategies.
 
 type TxPool struct {
 	client *CrossSporkClient
-	pool   map[flow.Identifier]struct{}
+	pool   map[gethCommon.Hash]gethTypes.Transaction
 }
 
-func (t *TxPool) Send(ctx context.Context, tx flow.Transaction) (*flow.TransactionResult, error) {
-	if err := t.client.SendTransaction(ctx, tx); err != nil {
+func (t *TxPool) Send(ctx context.Context, flowTx flow.Transaction, evmTx gethTypes.Transaction) (*flow.TransactionResult, error) {
+	if err := t.client.SendTransaction(ctx, flowTx); err != nil {
 		return nil, err
 	}
 
-	t.pool[tx.ID()] = struct{}{}
+	// add to pool
+	t.pool[evmTx.Hash()] = evmTx
 
 	const fetchInterval = time.Millisecond * 500
+	const fetchTimeout = time.Minute * 3
 	ticker := time.NewTicker(fetchInterval)
+	timeout := time.NewTimer(fetchTimeout)
+
+	defer func() {
+		delete(t.pool, evmTx.Hash())
+		timeout.Stop()
+		ticker.Stop()
+	}()
+
 	select {
 	case <-ticker.C:
-		res, err := t.client.GetTransactionResult(ctx, tx.ID())
+		res, err := t.client.GetTransactionResult(ctx, flowTx.ID())
 		if err != nil {
-			return nil, err
+			return nil, fmt.Errorf("failed to retrieve flow transaction result %s: %w", flowTx.ID(), err)
 		}
 		if res.Status > flow.TransactionStatusPending {
-			ticker.Stop()
-			delete(t.pool, tx.ID())
 			return res, nil
 		}
+	case <-timeout.C:
+		return nil, fmt.Errorf("failed to retrieve evm transaction result: %s, flow id: %s", evmTx.Hash(), flowTx.ID())
 	}
 
 	return nil, nil
