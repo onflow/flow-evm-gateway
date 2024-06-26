0cefb974e6d1c9d45a1e5e75b0b26004239afb49
Ardit Marku <markoupetr@gmail.com>
2024-06-26 09:56:44 +0300

    Rework transaction validation unit test to table tests structure

--- models/transaction_validation_test.go
+++ models/transaction_validation_test.go
@@ -12,170 +12,133 @@ import (
 )
 
 func TestValidateTransaction(t *testing.T) {
-	t.Parallel()
+	validToAddress := common.HexToAddress("0x000000000000000000000000000000000000dEaD")
+	zeroToAddress := common.HexToAddress("0x0000000000000000000000000000000000000000")
 
-	t.Run("valid transaction", func(t *testing.T) {
-		t.Parallel()
+	smallContractPayload, err := hex.DecodeString("c6888fa1")
+	require.NoError(t, err)
 
-		to := common.HexToAddress("0x000000000000000000000000000000000000dEaD")
-		tx := types.NewTx(
-			&types.LegacyTx{
-				Nonce:    1,
-				To:       &to,
-				Value:    big.NewInt(0),
-				Gas:      25_000,
-				GasPrice: big.NewInt(0),
-				Data:     []byte{},
-			},
-		)
+	invalidTxData, err := hex.DecodeString("c6888f")
+	require.NoError(t, err)
 
-		err := ValidateTransaction(tx)
-		assert.NoError(t, err)
-	})
+	invalidTxDataLen, err := hex.DecodeString("c6888fa1000000000000000000000000000000000000000000000000000000000000ab")
+	require.NoError(t, err)
 
-	t.Run("send to 0 address", func(t *testing.T) {
-		t.Parallel()
+	tests := map[string]struct {
+		tx     *types.Transaction
+		valid  bool
+		errMsg string
+	}{
+		"valid transaction": {
+			tx: types.NewTx(
+				&types.LegacyTx{
+					Nonce:    1,
+					To:       &validToAddress,
+					Value:    big.NewInt(0),
+					Gas:      25_000,
+					GasPrice: big.NewInt(0),
+					Data:     []byte{},
+				},
+			),
+			valid:  true,
+			errMsg: "",
+		},
+		"send to 0 address": {
+			tx: types.NewTx(
+				&types.LegacyTx{
+					Nonce:    1,
+					To:       &zeroToAddress,
+					Value:    big.NewInt(0),
+					Gas:      25_000,
+					GasPrice: big.NewInt(0),
+					Data:     []byte{},
+				},
+			),
+			valid:  false,
+			errMsg: "transaction recipient is the zero address",
+		},
+		"create empty contract (no value)": {
+			tx: types.NewTx(
+				&types.LegacyTx{
+					Nonce:    1,
+					To:       nil,
+					Value:    big.NewInt(0),
+					Gas:      53_000,
+					GasPrice: big.NewInt(0),
+					Data:     []byte{},
+				},
+			),
+			valid:  false,
+			errMsg: "transaction will create a contract with empty code",
+		},
+		"create empty contract (with value)": {
+			tx: types.NewTx(
+				&types.LegacyTx{
+					Nonce:    1,
+					To:       nil,
+					Value:    big.NewInt(150),
+					Gas:      53_000,
+					GasPrice: big.NewInt(0),
+					Data:     []byte{},
+				},
+			),
+			valid:  false,
+			errMsg: "transaction will create a contract with value but empty code",
+		},
+		"small payload for create": {
+			tx: types.NewTx(
+				&types.LegacyTx{
+					Nonce:    1,
+					To:       nil,
+					Value:    big.NewInt(150),
+					Gas:      53_000,
+					GasPrice: big.NewInt(0),
+					Data:     smallContractPayload,
+				},
+			),
+			valid:  false,
+			errMsg: "transaction will create a contract, but the payload is suspiciously small (4 bytes)",
+		},
+		"tx data length < 4": {
+			tx: types.NewTx(
+				&types.LegacyTx{
+					Nonce:    1,
+					To:       &validToAddress,
+					Value:    big.NewInt(150),
+					Gas:      153_000,
+					GasPrice: big.NewInt(0),
+					Data:     invalidTxData,
+				},
+			),
+			valid:  false,
+			errMsg: "transaction data is not valid ABI (missing the 4 byte call prefix)",
+		},
+		"tx data (excluding function selector) not divisible by 32": {
+			tx: types.NewTx(
+				&types.LegacyTx{
+					Nonce:    1,
+					To:       &validToAddress,
+					Value:    big.NewInt(150),
+					Gas:      153_000,
+					GasPrice: big.NewInt(0),
+					Data:     invalidTxDataLen,
+				},
+			),
+			valid:  false,
+			errMsg: "transaction data is not valid ABI (length should be a multiple of 32 (was 31))",
+		},
+	}
 
-		to := common.HexToAddress("0x0000000000000000000000000000000000000000")
-		tx := types.NewTx(
-			&types.LegacyTx{
-				Nonce:    1,
-				To:       &to,
-				Value:    big.NewInt(0),
-				Gas:      25_000,
-				GasPrice: big.NewInt(0),
-				Data:     []byte{},
-			},
-		)
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			err := ValidateTransaction(tc.tx)
+			if tc.valid {
+				require.NoError(t, err)
+			} else {
+				require.Error(t, err)
+				assert.ErrorContains(t, err, tc.errMsg)
+			}
+		})
+	}
 
-		err := ValidateTransaction(tx)
-		require.Error(t, err)
-		assert.ErrorContains(t, err, "transaction recipient is the zero address")
-	})
-
-	t.Run("create empty contract (no value)", func(t *testing.T) {
-		t.Parallel()
-
-		tx := types.NewTx(
-			&types.LegacyTx{
-				Nonce:    1,
-				To:       nil,
-				Value:    big.NewInt(0),
-				Gas:      53_000,
-				GasPrice: big.NewInt(0),
-				Data:     []byte{},
-			},
-		)
-
-		err := ValidateTransaction(tx)
-		require.Error(t, err)
-		assert.ErrorContains(
-			t,
-			err,
-			"transaction will create a contract with empty code",
-		)
-	})
-
-	t.Run("create empty contract (with value)", func(t *testing.T) {
-		t.Parallel()
-
-		tx := types.NewTx(
-			&types.LegacyTx{
-				Nonce:    1,
-				To:       nil,
-				Value:    big.NewInt(150),
-				Gas:      53_000,
-				GasPrice: big.NewInt(0),
-				Data:     []byte{},
-			},
-		)
-
-		err := ValidateTransaction(tx)
-		require.Error(t, err)
-		assert.ErrorContains(
-			t,
-			err,
-			"transaction will create a contract with value but empty code",
-		)
-	})
-
-	t.Run("small payload for create", func(t *testing.T) {
-		t.Parallel()
-
-		data, err := hex.DecodeString("c6888fa1")
-		require.NoError(t, err)
-
-		tx := types.NewTx(
-			&types.LegacyTx{
-				Nonce:    1,
-				To:       nil,
-				Value:    big.NewInt(150),
-				Gas:      53_000,
-				GasPrice: big.NewInt(0),
-				Data:     data,
-			},
-		)
-
-		err = ValidateTransaction(tx)
-		require.Error(t, err)
-		assert.ErrorContains(
-			t,
-			err,
-			"transaction will create a contract, but the payload is suspiciously small (4 bytes)",
-		)
-	})
-
-	t.Run("tx data length < 4", func(t *testing.T) {
-		t.Parallel()
-
-		data, err := hex.DecodeString("c6888f")
-		require.NoError(t, err)
-
-		to := common.HexToAddress("0x000000000000000000000000000000000000dEaD")
-		tx := types.NewTx(
-			&types.LegacyTx{
-				Nonce:    1,
-				To:       &to,
-				Value:    big.NewInt(150),
-				Gas:      153_000,
-				GasPrice: big.NewInt(0),
-				Data:     data,
-			},
-		)
-
-		err = ValidateTransaction(tx)
-		require.Error(t, err)
-		assert.ErrorContains(
-			t,
-			err,
-			"transaction data is not valid ABI (missing the 4 byte call prefix)",
-		)
-	})
-
-	t.Run("tx data (excluding function selector) not divisible by 32", func(t *testing.T) {
-		t.Parallel()
-
-		data, err := hex.DecodeString("c6888fa1000000000000000000000000000000000000000000000000000000000000ab")
-		require.NoError(t, err)
-
-		to := common.HexToAddress("0x000000000000000000000000000000000000dEaD")
-		tx := types.NewTx(
-			&types.LegacyTx{
-				Nonce:    1,
-				To:       &to,
-				Value:    big.NewInt(150),
-				Gas:      153_000,
-				GasPrice: big.NewInt(0),
-				Data:     data,
-			},
-		)
-
-		err = ValidateTransaction(tx)
-		require.Error(t, err)
-		assert.ErrorContains(
-			t,
-			err,
-			"transaction data is not valid ABI (length should be a multiple of 32 (was 31))",
-		)
-	})
 }
