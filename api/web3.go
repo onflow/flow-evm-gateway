5fb97a02278f9c49588519f6c01701b8badd17f2
Ardit Marku <markoupetr@gmail.com>
2024-07-01 18:08:49 +0300

    Add the Version variable in web3.go for injecting the running client version

--- api/web3.go
+++ api/web3.go
@@ -1,16 +1,20 @@
 package api
 
 import (
+	"fmt"
+
 	"github.com/onflow/go-ethereum/common/hexutil"
 	"github.com/onflow/go-ethereum/crypto"
 )
+
+var Version = "development"
 
 // Web3API offers helper utils
 type Web3API struct{}
 
 // ClientVersion returns the node name
 func (s *Web3API) ClientVersion() string {
-	return "flow-evm-gateway@v0.1.0"
+	return fmt.Sprintf("flow-evm-gateway@%s", Version)
 }
 
 // Sha3 applies the ethereum sha3 implementation on the input.
