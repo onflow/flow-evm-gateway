35b645228e7dd580c1d8bda2194f15eec09b922e
sideninja <75445744+sideninja@users.noreply.github.com>
2024-07-03 17:35:26 +0200

    update streaming to use new pattern

--- api/stream.go
+++ api/stream.go
@@ -2,13 +2,10 @@ package api
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"math/big"
-	"reflect"
 
 	"github.com/onflow/flow-go/engine"
-	"github.com/onflow/flow-go/engine/access/subscription"
 	"github.com/onflow/flow-go/fvm/evm/types"
 	"github.com/onflow/go-ethereum/common"
 	"github.com/onflow/go-ethereum/common/hexutil"
@@ -22,7 +19,6 @@ import (
 	"github.com/onflow/flow-evm-gateway/models"
 	"github.com/onflow/flow-evm-gateway/services/logs"
 	"github.com/onflow/flow-evm-gateway/storage"
-	storageErrs "github.com/onflow/flow-evm-gateway/storage/errors"
 )
 
 // subscriptionBufferLimit is a constant that represents the buffer limit for subscriptions.
@@ -68,7 +64,7 @@ func NewStreamAPI(
 
 // NewHeads send a notification each time a new block is appended to the chain.
 func (s *StreamAPI) NewHeads(ctx context.Context) (*rpc.Subscription, error) {
-	return s.new(ctx, func(notifier *rpc.Notifier, sub *rpc.Subscription) func(any) error {
+	return s.newSubscription(ctx, func(notifier *rpc.Notifier, sub *rpc.Subscription) func(any) error {
 		return func(data any) error {
 			block, ok := data.(*types.Block)
 			if !ok {
@@ -100,211 +96,59 @@ func (s *StreamAPI) NewHeads(ctx context.Context) (*rpc.Subscription, error) {
 // transaction enters the transaction pool. If fullTx is true the full tx is
 // sent to the client, otherwise the hash is sent.
 func (s *StreamAPI) NewPendingTransactions(ctx context.Context, fullTx *bool) (*rpc.Subscription, error) {
-	sub, err := s.newSubscription(
-		ctx,
-		s.transactionsBroadcaster,
-		func(ctx context.Context, height uint64) (interface{}, error) {
-			block, err := s.blocks.GetByHeight(height)
-			if err != nil {
-				if errors.Is(err, storageErrs.ErrNotFound) {
-					// make sure to wrap in not found error as the streamer expects it
-					return nil, errors.Join(subscription.ErrBlockNotReady, err)
-				}
-				return nil, fmt.Errorf("failed to get block at height: %d: %w", height, err)
+	return s.newSubscription(ctx, func(notifier *rpc.Notifier, sub *rpc.Subscription) func(any) error {
+		return func(data any) error {
+			tx, ok := data.(models.Transaction)
+			if !ok {
+				return fmt.Errorf("invalid data sent to pending transaction subscription")
 			}
 
-			hashes := make([]common.Hash, len(block.TransactionHashes))
-			txs := make([]*Transaction, len(block.TransactionHashes))
-
-			for i, hash := range block.TransactionHashes {
-
-				tx, err := s.transactions.Get(hash)
-				if err != nil {
-					if errors.Is(err, storageErrs.ErrNotFound) {
-						// make sure to wrap in not found error as the streamer expects it
-						return nil, errors.Join(subscription.ErrBlockNotReady, err)
-					}
-					return nil, fmt.Errorf("failed to get tx with hash: %s at height: %d: %w", hash, height, err)
-				}
-
-				rcp, err := s.receipts.GetByTransactionID(hash)
-				if err != nil {
-					if errors.Is(err, storageErrs.ErrNotFound) {
-						// make sure to wrap in not found error as the streamer expects it
-						return nil, errors.Join(subscription.ErrBlockNotReady, err)
-					}
-					return nil, fmt.Errorf("failed to get receipt with hash: %s at height: %d: %w", hash, height, err)
-				}
-
-				h := tx.Hash()
-
-				t, err := NewTransaction(tx, *rcp, *s.config)
-				if err != nil {
-					return nil, err
-				}
-
-				hashes[i] = h
-				txs[i] = t
-			}
-
+			var res any
 			if fullTx != nil && *fullTx {
-				return txs, nil
+				if r, err := NewTransaction(tx, s.config.EVMNetworkID); err != nil {
+					return err
+				} else {
+					res = r
+				}
+			} else {
+				res = tx.Hash()
 			}
-			return hashes, nil
-		},
-	)
-	l := s.logger.With().Str("subscription-id", string(sub.ID)).Logger()
-	if err != nil {
-		l.Error().Err(err).Msg("error creating a new pending transactions subscription")
-		return nil, err
-	}
 
-	l.Info().Msg("new pending transactions subscription created")
-	return sub, nil
+			return notifier.Notify(sub.ID, res)
+		}
+	})
 }
 
 // Logs creates a subscription that fires for all new log that match the given filter criteria.
 func (s *StreamAPI) Logs(ctx context.Context, criteria filters.FilterCriteria) (*rpc.Subscription, error) {
-	filter, err := logs.NewFilterCriteria(criteria.Addresses, criteria.Topics)
+	logCriteria, err := logs.NewFilterCriteria(criteria.Addresses, criteria.Topics)
 	if err != nil {
 		return nil, fmt.Errorf("failed to crete log subscription filter: %w", err)
 	}
 
-	sub, err := s.newSubscription(
-		ctx,
-		s.logsBroadcaster,
-		func(ctx context.Context, height uint64) (interface{}, error) {
-			if criteria.ToBlock != nil && height > criteria.ToBlock.Uint64() {
-				return nil, nil
-			}
-			if criteria.FromBlock != nil && height < criteria.FromBlock.Uint64() {
-				return nil, nil
+	return s.newSubscription(ctx, func(notifier *rpc.Notifier, sub *rpc.Subscription) func(any) error {
+		return func(data any) error {
+			allLogs, ok := data.([]*gethTypes.Log)
+			if !ok {
+				return fmt.Errorf("invalid data sent to log subscription")
 			}
 
-			block, err := s.blocks.GetByHeight(height)
-			if err != nil {
-				if errors.Is(err, storageErrs.ErrNotFound) {
-					// make sure to wrap in not found error as the streamer expects it
-					return nil, errors.Join(subscription.ErrBlockNotReady, err)
+			for _, log := range allLogs {
+				if !logs.ExactMatch(log, logCriteria) {
+					continue
 				}
-				return nil, fmt.Errorf("failed to get block at height: %d: %w", height, err)
-			}
 
-			id, err := block.Hash()
-			if err != nil {
-				return nil, err
-			}
-
-			// todo change this to height filter so we don't have to get the same block twice, once for height and then for id
-
-			return logs.NewIDFilter(id, *filter, s.blocks, s.receipts).Match()
-		},
-	)
-	l := s.logger.With().Str("subscription-id", string(sub.ID)).Logger()
-	if err != nil {
-		l.Error().Err(err).Msg("failed creating logs subscription")
-		return nil, err
-	}
-
-	l.Info().Msg("new logs subscription created")
-
-	return sub, nil
-}
-
-// newSubscription creates a new subscription for receiving events from the broadcaster.
-// The data adapter function is used to transform the raw data received from the broadcaster
-// to comply with requested RPC API response schema.
-func (s *StreamAPI) newSubscription(
-	ctx context.Context,
-	broadcaster *engine.Broadcaster,
-	getData subscription.GetDataByHeightFunc,
-) (*rpc.Subscription, error) {
-	if err := rateLimit(ctx, s.ratelimiter, s.logger); err != nil {
-		return nil, err
-	}
-
-	notifier, supported := rpc.NotifierFromContext(ctx)
-	if !supported {
-		return nil, rpc.ErrNotificationsUnsupported
-	}
-
-	height, err := s.blocks.LatestEVMHeight()
-	if err != nil {
-		return nil, err
-	}
-	height += 1 // subscribe to the next new event which will produce next height
-
-	sub := subscription.NewHeightBasedSubscription(subscriptionBufferLimit, height, getData)
-
-	rpcSub := notifier.CreateSubscription()
-	rpcSub.ID = rpc.ID(sub.ID()) // make sure ids are unified
-
-	l := s.logger.With().Str("subscription-id", string(rpcSub.ID)).Logger()
-	l.Info().Uint64("evm-height", height).Msg("new subscription created")
-
-	go subscription.NewStreamer(
-		s.logger.With().Str("component", "streamer").Logger(),
-		broadcaster,
-		s.config.StreamTimeout,
-		s.config.StreamLimit,
-		sub,
-	).Stream(context.Background()) // todo investigate why the passed in context is canceled so quickly
-
-	// stream all available data
-	go streamData(notifier, rpcSub, sub, l)
-
-	return rpcSub, nil
-}
-
-// streamData runs a loop listening on a chanel for any new data and if
-// received it streams it to the rpc client.
-func streamData(
-	notifier *rpc.Notifier,
-	rpcSub *rpc.Subscription,
-	sub *subscription.HeightBasedSubscription,
-	l zerolog.Logger,
-) {
-	defer sub.Close()
-
-	for {
-		select {
-		case data, open := <-sub.Channel():
-			if !open {
-				l.Debug().Msg("subscription channel closed")
-				return
-			}
-
-			// if we receive slice of data we stream each item separately
-			// as it's expected by the rpc specification
-			v := reflect.ValueOf(data)
-			if v.Kind() == reflect.Slice {
-				for i := 0; i < v.Len(); i++ {
-					item := v.Index(i).Interface()
-					sendData(notifier, rpcSub.ID, item, l)
+				if err := notifier.Notify(sub.ID, log); err != nil {
+					return err
 				}
-			} else {
-				sendData(notifier, rpcSub.ID, data, l)
 			}
 
-		case err := <-rpcSub.Err():
-			l.Debug().Err(err).Msg("client unsubscribed")
-			return
-		case <-notifier.Closed():
-			l.Debug().Msg("client unsubscribed deprecated method")
-			return
+			return nil
 		}
-	}
+	})
 }
 
-func sendData(notifier *rpc.Notifier, id rpc.ID, data any, l zerolog.Logger) {
-	l.Debug().Str("subscription-id", string(id)).Any("data", data).Msg("notifying new event")
-
-	if err := notifier.Notify(id, data); err != nil {
-		l.Err(err).Msg("failed to notify")
-	}
-}
-
-func (s *StreamAPI) new(
+func (s *StreamAPI) newSubscription(
 	ctx context.Context,
 	callback func(notifier *rpc.Notifier, sub *rpc.Subscription) func(any) error,
 ) (*rpc.Subscription, error) {
