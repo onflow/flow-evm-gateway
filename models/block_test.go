5e6bc5168bd6614e6ae554c4fa1a2995709c32a8
sideninja <75445744+sideninja@users.noreply.github.com>
2024-07-24 15:11:48 +0200

    add backward compatible test

--- models/block_test.go
+++ models/block_test.go
@@ -13,7 +13,7 @@ import (
 )
 
 func Test_DecodeBlockExecutedEvent(t *testing.T) {
-	block := &types.Block{
+	gethBlock := &types.Block{
 		ParentBlockHash:     gethCommon.HexToHash("0x1"),
 		Height:              100,
 		TotalSupply:         big.NewInt(100),
@@ -21,12 +21,42 @@ func Test_DecodeBlockExecutedEvent(t *testing.T) {
 		TransactionHashRoot: gethCommon.HexToHash("0x3"),
 		TotalGasUsed:        uint64(30),
 	}
-	ev := events.NewBlockEvent(block)
+	block := &Block{Block: gethBlock}
+	ev := events.NewBlockEvent(gethBlock)
 
 	encEv, err := ev.Payload.ToCadence(flowGo.Previewnet)
 	require.NoError(t, err)
 
 	decBlock, err := decodeBlockEvent(encEv)
 	require.NoError(t, err)
-	assert.Equal(t, block, decBlock)
+
+	assert.Equal(t, block.Block, gethBlock)
+	h1, err := block.Hash()
+	require.NoError(t, err)
+	h2, err := decBlock.Hash()
+	require.NoError(t, err)
+	assert.Equal(t, h1, h2)
+}
+
+func Test_Hash(t *testing.T) {
+	// we fix the hash calculation for bellow block, so we can detect
+	// any breaking changes in hash calculation or block structure
+	// coming from changes in EVM Core (flow-go), we should be aware of changes
+	// and this test makes sure we are, if changes occur it means they break backward
+	// compatibility when calculating hashes for older blocks.
+	const hash = "0x416d070745756c79d23d3bda31020ece8326ac7e862c2f5440c9a0661edd3769"
+
+	gethBlock := &types.Block{
+		ParentBlockHash:     gethCommon.HexToHash("0x1"),
+		Height:              100,
+		TotalSupply:         big.NewInt(100),
+		ReceiptRoot:         gethCommon.HexToHash("0x2"),
+		TransactionHashRoot: gethCommon.HexToHash("0x3"),
+		TotalGasUsed:        uint64(30),
+	}
+	block := &Block{Block: gethBlock}
+
+	h, err := block.Hash()
+	require.NoError(t, err)
+	require.Equal(t, hash, h.String())
 }
