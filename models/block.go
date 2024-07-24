f66bb1f78687ca3a88e76c214cea508b2bd18a80
sideninja <75445744+sideninja@users.noreply.github.com>
2024-07-24 13:13:44 +0200

    change block type implementation

--- models/block.go
+++ models/block.go
@@ -8,6 +8,7 @@ import (
 	"github.com/onflow/flow-go/fvm/evm/events"
 	"github.com/onflow/flow-go/fvm/evm/types"
 	gethCommon "github.com/onflow/go-ethereum/common"
+	"github.com/onflow/go-ethereum/rlp"
 )
 
 var (
@@ -20,14 +21,23 @@ var (
 
 var GenesisBlock = &Block{Block: types.GenesisBlock}
 
+func NewBlockFromBytes(data []byte) (*Block, error) {
+	var b *Block
+	return b, rlp.DecodeBytes(data, b)
+}
+
 type Block struct {
 	*types.Block
 	TransactionHashes []gethCommon.Hash
 }
 
-// decodeBlock takes a cadence event that contains executed block payload and
+func (b *Block) ToBytes() ([]byte, error) {
+	return rlp.EncodeToBytes(b)
+}
+
+// decodeBlockEvent takes a cadence event that contains executed block payload and
 // decodes it into the Block type.
-func decodeBlock(event cadence.Event) (*Block, error) {
+func decodeBlockEvent(event cadence.Event) (*Block, error) {
 	payload, err := events.DecodeBlockEventPayload(event)
 	if err != nil {
 		return nil, fmt.Errorf("failed to cadence decode block [%s]: %w", event.String(), err)
