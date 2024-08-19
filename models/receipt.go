f9aa388ed193e1dcadb9413c1372786ab20d0125
sideninja <75445744+sideninja@users.noreply.github.com>
2024-08-19 14:24:07 +0200

    add precompiled calls to receipt

--- models/receipt.go
+++ models/receipt.go
@@ -31,6 +31,7 @@ type StorageReceipt struct {
 	BlockNumber       *big.Int         `json:"blockNumber,omitempty"`
 	TransactionIndex  uint             `json:"transactionIndex"`
 	RevertReason      []byte           `json:"revertReason"`
+	PrecompiledCalls  []byte
 }
 
 func (sr *StorageReceipt) ToGethReceipt() *gethTypes.Receipt {
