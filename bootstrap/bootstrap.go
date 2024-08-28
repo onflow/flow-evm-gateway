31680177bcded83361f284a6c4c344b2348e3eea
sideninja <75445744+sideninja@users.noreply.github.com>
2024-08-28 14:11:25 +0200

    improve bootstrap with helpers

--- bootstrap/bootstrap.go
+++ bootstrap/bootstrap.go
@@ -2,6 +2,7 @@ package bootstrap
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"math"
 	"time"
@@ -16,6 +17,7 @@ import (
 	"github.com/onflow/flow-evm-gateway/config"
 	"github.com/onflow/flow-evm-gateway/metrics"
 	"github.com/onflow/flow-evm-gateway/models"
+	errs "github.com/onflow/flow-evm-gateway/models/errors"
 	"github.com/onflow/flow-evm-gateway/services/ingestion"
 	"github.com/onflow/flow-evm-gateway/services/requester"
 	"github.com/onflow/flow-evm-gateway/services/traces"
@@ -52,66 +54,61 @@ type Bootstrap struct {
 }
 
 func New(config *config.Config) (*Bootstrap, error) {
-	logger := zerolog.New(config.LogWriter).With().Timestamp().Logger()
-	logger = logger.Level(config.LogLevel)
-	logger.Info().Msg("starting up the EVM gateway")
+	logger := zerolog.New(config.LogWriter).
+		With().Timestamp().Logger().Level(config.LogLevel)
 
-	// create pebble storage from the provided database root directory
-	store, err := pebble.New(config.DatabaseDir, logger)
+	client, err := setupCrossSporkClient(config, logger)
 	if err != nil {
 		return nil, err
 	}
 
-	// create access client with cross-spork capabilities
-	currentSporkClient, err := grpc.NewClient(config.AccessNodeHost)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create client connection for host: %s, with error: %w", config.AccessNodeHost, err)
-	}
-
-	// if we provided access node previous spork hosts add them to the client
-	pastSporkClients := make([]access.Client, len(config.AccessNodePreviousSporkHosts))
-	for i, host := range config.AccessNodePreviousSporkHosts {
-		grpcClient, err := grpc.NewClient(host)
-		if err != nil {
-			return nil, fmt.Errorf("failed to create client connection for host: %s, with error: %w", host, err)
-		}
-
-		pastSporkClients[i] = grpcClient
-	}
-
-	// initialize cross spork client to the access nodes
-	client, err := requester.NewCrossSporkClient(
-		currentSporkClient,
-		pastSporkClients,
-		logger,
-		config.FlowNetworkID,
-	)
+	storages, err := setupStorage(config, logger)
 	if err != nil {
 		return nil, err
 	}
 
 	return &Bootstrap{
 		publishers: &Publishers{
-			Block:       nil,
-			Transaction: nil,
-			Logs:        nil,
+			Block:       models.NewPublisher(),
+			Transaction: models.NewPublisher(),
+			Logs:        models.NewPublisher(),
 		},
-		storages: &Storages{
-			Storage:      store,
-			Blocks:       pebble.NewBlocks(store, config.FlowNetworkID),
-			Transactions: pebble.NewTransactions(store),
-			Receipts:     pebble.NewReceipts(store),
-			Accounts:     pebble.NewAccounts(store),
-			Traces:       pebble.NewTraces(store),
-		},
-		logger: logger,
-		config: config,
-		client: client,
+		storages: storages,
+		logger:   logger,
+		config:   config,
+		client:   client,
 	}, nil
 }
 
 func (b *Bootstrap) StartEventIngestion(ctx context.Context) error {
 	b.logger.Info().Msg("bootstrap starting event ingestion")
+
+	// get latest cadence block from the network and the database
+	latestCadenceBlock, err := b.client.GetLatestBlock(context.Background(), false)
+	if err != nil {
+		return fmt.Errorf("failed to get latest cadence block: %w", err)
+	}
+
+	latestCadenceHeight, err := b.storages.Blocks.LatestCadenceHeight()
+	if err != nil {
+		return err
+	}
+
+	// make sure the provided block to start the indexing can be loaded
+	_, err = b.client.GetBlockHeaderByHeight(context.Background(), latestCadenceHeight)
+	if err != nil {
+		return fmt.Errorf(
+			"failed to get provided cadence height %d: %w",
+			latestCadenceHeight,
+			err,
+		)
+	}
+
+	b.logger.Info().
+		Uint64("start-cadence-height", latestCadenceHeight).
+		Uint64("latest-cadence-height", latestCadenceBlock.Height).
+		Uint64("missed-heights", latestCadenceBlock.Height-latestCadenceHeight).
+		Msg("indexing cadence height information")
 
 	// create event subscriber
 	subscriber := ingestion.NewRPCSubscriber(
@@ -356,6 +353,92 @@ func (b *Bootstrap) startEngine(
 
 	<-engine.Ready()
 	b.logger.Info().Msgf("%s engine strated successfully", name)
+}
+
+// setupCrossSporkClient sets up a cross-spork AN client.
+func setupCrossSporkClient(config *config.Config, logger zerolog.Logger) (*requester.CrossSporkClient, error) {
+	// create access client with cross-spork capabilities
+	currentSporkClient, err := grpc.NewClient(config.AccessNodeHost)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create client connection for host: %s, with error: %w", config.AccessNodeHost, err)
+	}
+
+	// if we provided access node previous spork hosts add them to the client
+	pastSporkClients := make([]access.Client, len(config.AccessNodePreviousSporkHosts))
+	for i, host := range config.AccessNodePreviousSporkHosts {
+		grpcClient, err := grpc.NewClient(host)
+		if err != nil {
+			return nil, fmt.Errorf("failed to create client connection for host: %s, with error: %w", host, err)
+		}
+
+		pastSporkClients[i] = grpcClient
+	}
+
+	// initialize cross spork client to the access nodes
+	client, err := requester.NewCrossSporkClient(
+		currentSporkClient,
+		pastSporkClients,
+		logger,
+		config.FlowNetworkID,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create cross spork client: %w", err)
+	}
+
+	return client, nil
+}
+
+// setupStorage creates storage and initializes it with configured starting cadence height
+// in case such a height doesn't already exist in the database.
+func setupStorage(config *config.Config, logger zerolog.Logger) (*Storages, error) {
+	// create pebble storage from the provided database root directory
+	store, err := pebble.New(config.DatabaseDir, logger)
+	if err != nil {
+		return nil, err
+	}
+
+	blocks := pebble.NewBlocks(store, config.FlowNetworkID)
+
+	client, err := setupCrossSporkClient(config, logger)
+	if err != nil {
+		return nil, err
+	}
+
+	// hard set the start cadence height, this is used when force reindexing
+	if config.ForceStartCadenceHeight != 0 {
+		logger.Warn().Uint64("height", config.ForceStartCadenceHeight).Msg("force setting starting Cadence height!!!")
+		if err := blocks.SetLatestCadenceHeight(config.ForceStartCadenceHeight, nil); err != nil {
+			return nil, err
+		}
+	}
+
+	// if database is not initialized require init height
+	if _, err := blocks.LatestCadenceHeight(); errors.Is(err, errs.ErrStorageNotInitialized) {
+		cadenceHeight := config.InitCadenceHeight
+		cadenceBlock, err := client.GetBlockHeaderByHeight(context.Background(), cadenceHeight)
+		if err != nil {
+			return nil, fmt.Errorf("could not fetch provided cadence height, make sure it's correct: %w", err)
+		}
+
+		if err := blocks.InitHeights(cadenceHeight, cadenceBlock.ID); err != nil {
+			return nil, fmt.Errorf(
+				"failed to init the database for block height: %d and ID: %s, with : %w",
+				cadenceHeight,
+				cadenceBlock.ID,
+				err,
+			)
+		}
+		logger.Info().Msgf("database initialized with cadence height: %d", cadenceHeight)
+	}
+
+	return &Storages{
+		Storage:      store,
+		Blocks:       blocks,
+		Transactions: pebble.NewTransactions(store),
+		Receipts:     pebble.NewReceipts(store),
+		Accounts:     pebble.NewAccounts(store),
+		Traces:       pebble.NewTraces(store),
+	}, nil
 }
 
 // Run will run complete bootstrap of the EVM gateway with all the engines.
