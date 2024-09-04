27e885d9f84a1289c44312ebd1decbfde1a6049f
sideninja <75445744+sideninja@users.noreply.github.com>
2024-09-04 21:04:49 +0200

    improve test with new bootstrap

--- tests/state_integration_test.go
+++ tests/state_integration_test.go
@@ -2,14 +2,11 @@ package tests
 
 import (
 	"context"
-	"encoding/hex"
+	"fmt"
 	"math/big"
 	"testing"
 	"time"
 
-	"github.com/onflow/cadence"
-	"github.com/onflow/cadence/encoding/json"
-	"github.com/onflow/flow-go-sdk/access/grpc"
 	"github.com/onflow/flow-go/fvm/evm/types"
 	flowGo "github.com/onflow/flow-go/model/flow"
 	"github.com/onflow/go-ethereum/common"
@@ -18,11 +15,8 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
+	"github.com/onflow/flow-evm-gateway/bootstrap"
 	"github.com/onflow/flow-evm-gateway/config"
-	"github.com/onflow/flow-evm-gateway/metrics"
-	"github.com/onflow/flow-evm-gateway/models"
-	"github.com/onflow/flow-evm-gateway/services/ingestion"
-	"github.com/onflow/flow-evm-gateway/services/requester"
 	"github.com/onflow/flow-evm-gateway/services/state"
 	"github.com/onflow/flow-evm-gateway/storage/pebble"
 )
@@ -56,83 +50,19 @@ func Test_StateExecution(t *testing.T) {
 		LogWriter:         zerolog.NewConsoleWriter(),
 	}
 
-	logger := zerolog.New(zerolog.NewConsoleWriter()).With().Timestamp().Logger()
-
-	store, err := pebble.New(cfg.DatabaseDir, logger)
+	b, err := bootstrap.New(cfg)
 	require.NoError(t, err)
 
-	blocks := pebble.NewBlocks(store, cfg.FlowNetworkID)
-	transactions := pebble.NewTransactions(store)
-	receipts := pebble.NewReceipts(store)
-	accounts := pebble.NewAccounts(store)
+	time.Sleep(1 * time.Second)
 
-	blocksPublisher := models.NewPublisher()
-	logsPublisher := models.NewPublisher()
+	require.NoError(t, b.StartEventIngestion(ctx))
+	require.NoError(t, b.StartAPIServer(ctx))
+	require.NoError(t, b.StartStateIndex(ctx))
 
-	currentSporkClient, err := grpc.NewClient(cfg.AccessNodeHost)
-	require.NoError(t, err)
-
-	client, err := requester.NewCrossSporkClient(
-		currentSporkClient,
-		nil,
-		logger,
-		cfg.FlowNetworkID,
-	)
-	require.NoError(t, err)
-
-	cadenceBlock, err := client.GetBlockHeaderByHeight(ctx, cfg.InitCadenceHeight)
-	require.NoError(t, err)
-
-	err = blocks.InitHeights(cfg.InitCadenceHeight, cadenceBlock.ID)
-	require.NoError(t, err)
-
-	subscriber := ingestion.NewRPCSubscriber(
-		client,
-		cfg.HeartbeatInterval,
-		cfg.FlowNetworkID,
-		logger,
-	)
-
-	eventEngine := ingestion.NewEventIngestionEngine(
-		subscriber,
-		store,
-		blocks,
-		receipts,
-		transactions,
-		accounts,
-		blocksPublisher,
-		logsPublisher,
-		logger,
-		metrics.NopCollector,
-	)
-
-	go func() {
-		err = eventEngine.Run(ctx)
-		if err != nil {
-			logger.Error().Err(err).Msg("event ingestion engine failed to run")
-			panic(err)
-		}
-	}()
-
-	// wait for ingestion engines to be ready
-	<-eventEngine.Ready()
-
-	stateEngine := state.NewStateEngine(
-		cfg,
-		pebble.NewLedger(store),
-		blocksPublisher,
-		blocks,
-		transactions,
-		receipts,
-		logger,
-	)
-	if err := stateEngine.Run(ctx); err != nil {
-		logger.Error().Err(err).Msg("state engine failed to run")
-		panic(err)
-	}
-
-	// wait for state engine to be ready
-	<-stateEngine.Ready()
+	blocks := b.Storages.Blocks
+	receipts := b.Storages.Receipts
+	store := b.Storages.Storage
+	requester := b.Requester
 
 	latest, err := blocks.LatestEVMHeight()
 	require.NoError(t, err)
@@ -153,52 +83,14 @@ func Test_StateExecution(t *testing.T) {
 	evmTx, _, err := evmSign(amount, 21000, eoaKey, 0, &testAddr, nil)
 	require.NoError(t, err)
 
-	// todo reuse
-	script := []byte(`import EVM from 0xf8d6e0586b0a20c7
-
-transaction(hexEncodedTx: String) {
-    let coa: &EVM.CadenceOwnedAccount
-
-    prepare(signer: auth(Storage) &Account) {
-        self.coa = signer.storage.borrow<&EVM.CadenceOwnedAccount>(
-            from: /storage/evm
-        ) ?? panic("Could not borrow reference to the COA!")
-    }
-
-    execute {
-        let txResult = EVM.run(
-            tx: hexEncodedTx.decodeHex(),
-            coinbase: self.coa.address()
-        )
-        assert(
-            txResult.status == EVM.Status.successful,
-            message: "evm_error=".concat(txResult.errorMessage).concat("\n")
-        )
-    }
-}
-`)
-
-	txData := hex.EncodeToString(evmTx)
-	hexEncodedTx, err := cadence.NewString(txData)
-	require.NoError(t, err)
-	arg, err := json.Encode(hexEncodedTx)
-	require.NoError(t, err)
-
-	latestBlock, err := client.GetLatestBlock(ctx, true)
-	require.NoError(t, err)
-
-	tx := flowGo.NewTransactionBody().
-		SetReferenceBlockID(flowGo.Identifier(latestBlock.ID)).
-		SetScript(script).
-		SetPayer(flowGo.Address(service.Address)).
-		SetProposalKey(flowGo.Address(service.Address), 0, 0).
-		AddArgument(arg).
-		AddAuthorizer(flowGo.Address(service.Address))
-
-	err = emu.SendTransaction(tx)
-	require.NoError(t, err)
-
 	time.Sleep(1 * time.Second)
+
+	fmt.Println("-------------------------")
+	hash, err := requester.SendRawTransaction(ctx, evmTx)
+	require.NoError(t, err)
+	require.NotEmpty(t, hash)
+	fmt.Println("-------------------------")
+	time.Sleep(1 * time.Second) // wait for tx to get ingested
 
 	latest, err = blocks.LatestEVMHeight()
 	require.NoError(t, err)
