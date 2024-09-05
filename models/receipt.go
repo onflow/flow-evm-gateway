b5f95cb42f78d16d31a973abe36d9918e697e3a7
sideninja <75445744+sideninja@users.noreply.github.com>
2024-09-05 16:37:30 +0200

    add compare receipts

--- models/receipt.go
+++ models/receipt.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"bytes"
 	"fmt"
 	"math/big"
 
@@ -67,6 +68,49 @@ func ReceiptsFromBytes(data []byte) ([]*Receipt, error) {
 		return nil, fmt.Errorf("failed to RLP-decode block receipts [%x] %w", data, err)
 	}
 	return receipts, nil
+}
+
+// EqualReceipts takes a geth Receipt type and EVM GW receipt and compares all the applicable values.
+func EqualReceipts(gethReceipt *gethTypes.Receipt, receipt *Receipt) bool {
+	// fail if any receipt or both are nil
+	if gethReceipt == nil || receipt == nil {
+		return false
+	}
+	// compare logs
+	if len(gethReceipt.Logs) != len(receipt.Logs) {
+		return false
+	}
+
+	// todo block data might not be present, investigate
+	for i, l := range gethReceipt.Logs {
+		rl := receipt.Logs[i]
+		if rl.BlockNumber != l.BlockNumber ||
+			rl.Removed != l.Removed ||
+			rl.TxHash.Cmp(l.TxHash) != 0 ||
+			rl.Address.Cmp(l.Address) != 0 ||
+			rl.BlockHash.Cmp(l.BlockHash) != 0 ||
+			rl.Index != l.Index ||
+			bytes.Equal(rl.Data, l.Data) == false ||
+			rl.TxIndex != l.TxIndex {
+			return false
+		}
+		// compare all topics
+		for j, t := range rl.Topics {
+			if t.Cmp(l.Topics[j]) != 0 {
+				return false
+			}
+		}
+	}
+
+	// compare all receipt data
+	return gethReceipt.TxHash.Cmp(receipt.TxHash) == 0 &&
+		gethReceipt.GasUsed == receipt.GasUsed &&
+		gethReceipt.CumulativeGasUsed == receipt.CumulativeGasUsed &&
+		gethReceipt.Type == receipt.Type &&
+		gethReceipt.ContractAddress.Cmp(receipt.ContractAddress) == 0 &&
+		gethReceipt.Status == receipt.Status &&
+		bytes.Equal(gethReceipt.Bloom.Bytes(), receipt.Bloom.Bytes())
+	// todo there are other fields, should we compare, do we have block number?
 }
 
 type BloomsHeight struct {
