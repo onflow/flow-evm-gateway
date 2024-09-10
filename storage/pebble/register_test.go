c5f3fb5a94a1b68b139d92c636e87ebe7386d1d7
sideninja <75445744+sideninja@users.noreply.github.com>
2024-09-10 23:05:05 +0200

    add register test at later heights

--- storage/pebble/register_test.go
+++ storage/pebble/register_test.go
@@ -1,7 +1,6 @@
 package pebble
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -55,100 +54,69 @@ func Test_RegisterSetGet(t *testing.T) {
 
 	runDB("multiple sets for different accounts and get at latest height", t, func(t *testing.T, db *Storage) {
 
-		// registers is map[height][owner][key] = value
-		registers := map[uint64]map[uint64]map[uint64]uint64{
-			10: {
-				100: {
-					500: 1000,
-					502: 2000,
-					504: 3000,
-				},
-				101: {
-					500: 3003,
-					502: 2005,
-					506: 4002,
-				},
-				102: {
-					500: 4004,
-				},
-			},
-			11: {
-				100: {
-					500: 1004,
-				},
-				102: {
-					505: 4003,
-				},
-			},
-			12: {
-				104: {
-					500: 1000,
-				},
-			},
-			13: {
-				100: {
-					500: 1006,
-				},
-			},
-			15: {
-				102: {
-					505: 4004,
-				},
-			},
-		}
+		height1 := uint64(1)
+		owner11 := uint64Bytes(101)
+		key11 := uint64Bytes(500)
+		val11 := uint64Bytes(1001)
+		key15 := uint64Bytes(500)
+		val15 := uint64Bytes(2002)
 
-		for height, a := range registers {
-			for owner, b := range a {
-				for key, value := range b {
-					owner := uint64Bytes(owner)
-					key := uint64Bytes(key)
-					val := uint64Bytes(value)
+		owner21 := uint64Bytes(105)
+		key21 := uint64Bytes(600)
+		val21 := uint64Bytes(1002)
+		key22 := uint64Bytes(500)
+		val22 := uint64Bytes(2002)
 
-					reg := NewRegister(db, height)
-					err := reg.SetValue(owner, key, val)
-					require.NoError(t, err)
-				}
-			}
-		}
+		reg := NewRegister(db, height1)
+		err := reg.SetValue(owner11, key11, val11)
+		require.NoError(t, err)
 
-		// expected is same map as registers but with heights after they were set
-		// build up expected map which is used to verify results
-		// it populates all the heights even if no value was set at that height,
-		// it just uses last set value for register, this way we can make sure the
-		// heights are correctly loaded.
-		expected := make(map[uint64]map[uint64]map[uint64]uint64)
-		for i := 5; i < 20; i++ {
-			h := uint64(i)
-			if i == 5 {
-				expected[h] = make(map[uint64]map[uint64]uint64)
-			} else { // copy previous values over to next height
-				expected[h] = expected[h-1]
-			}
-			// update any new data
-			for owner, a := range registers[h] {
-				expected[h][owner] = make(map[uint64]uint64)
-				for key, val := range a {
-					expected[h][owner][key] = val
-				}
-			}
-		}
+		height2 := uint64(3)
+		reg = NewRegister(db, height2)
+		err = reg.SetValue(owner21, key21, val21)
+		require.NoError(t, err)
+		err = reg.SetValue(owner21, key22, val22)
+		require.NoError(t, err)
 
-		// make sure registers are correct even at the heights they weren't changed
-		for height, a := range expected {
-			reg := NewRegister(db, height)
+		height3 := uint64(5)
+		reg = NewRegister(db, height3)
+		err = reg.SetValue(owner11, key15, val15)
+		require.NoError(t, err)
+		err = reg.SetValue(owner21, key22, val22)
+		require.NoError(t, err)
 
-			for owner, b := range a {
-				for key, val := range b {
-					o := uint64Bytes(owner)
-					k := uint64Bytes(key)
-					v := uint64Bytes(val)
+		reg = NewRegister(db, uint64(0))
+		// not found
+		val, err := reg.GetValue(owner11, key11)
+		require.Nil(t, err)
+		require.Nil(t, val)
 
-					retVal, err := reg.GetValue(o, k)
-					require.NoError(t, err)
-					require.Equal(t, v, retVal, fmt.Sprintf("height: %d, owner: %d, key: %d, value: %d", height, owner, key, val))
-				}
-			}
-		}
+		val, err = reg.GetValue(owner21, key21)
+		require.NoError(t, err)
+		require.Nil(t, val)
+
+		reg = NewRegister(db, uint64(1))
+		val, err = reg.GetValue(owner11, key11)
+		require.NoError(t, err)
+		require.Equal(t, val11, val)
+
+		reg = NewRegister(db, uint64(2))
+		val, err = reg.GetValue(owner11, key11)
+		require.NoError(t, err)
+		require.Equal(t, val11, val)
+
+		reg = NewRegister(db, uint64(3))
+		val, err = reg.GetValue(owner11, key11)
+		require.NoError(t, err)
+		require.Equal(t, val11, val)
+
+		reg = NewRegister(db, uint64(5))
+		val, err = reg.GetValue(owner11, key15)
+		require.NoError(t, err)
+		require.Equal(t, val15, val)
+
+		// todo write more examples
+
 	})
 
 }
