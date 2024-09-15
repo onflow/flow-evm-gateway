a4e500c06244d9644815f89ef44f9d1790b9428c
sideninja <75445744+sideninja@users.noreply.github.com>
2024-09-15 12:46:07 +0200

    extend storage contract to have logs emitted and improve the test

--- tests/web3js/cadence_arch_env_test.js
+++ tests/web3js/cadence_arch_env_test.js
@@ -4,143 +4,130 @@ const conf = require('./config')
 const helpers = require('./helpers')
 const web3 = conf.web3
 
-it('should successfully call cadence arch functions and block environment functions', async () => {
+describe('calls cadence arch functions and block environment functions', async () => {
     let deployed = await helpers.deployContract('storage')
     let contractAddress = deployed.receipt.contractAddress
+    let methods = deployed.contract.methods
 
-    // submit a transaction that calls blockNumber()
-    let blockNumberData = deployed.contract.methods.blockNumber().encodeABI()
-    let res = await helpers.signAndSend({
-        from: conf.eoa.address,
-        to: contractAddress,
-        data: blockNumberData,
-        value: '0',
-        gasPrice: conf.minGasPrice,
+    async function testEmitTx(method) {
+        let res = await helpers.signAndSend({
+            from: conf.eoa.address,
+            to: contractAddress,
+            data: method.encodeABI(),
+            value: '0',
+            gasPrice: conf.minGasPrice,
+        })
+        assert.equal(res.receipt.status, conf.successStatus)
+        assert.equal(res.receipt.logs.length, 1)
+
+        return res
+    }
+
+    async function testCall(method) {
+        let block = await web3.eth.getBlock('latest')
+
+        let value = await web3.eth.call({
+            to: contractAddress,
+            data: method.encodeABI()
+        }, block.number)
+
+        assert.isAbove(value.length, 0)
+
+        return {
+            value: value,
+            block: block,
+        }
+    }
+
+    it('calls blockNumber', async () => {
+        await testEmitTx(methods.emitBlockNumber())
+
+        let res = await testCall(methods.blockNumber())
+        assert.equal(
+            web3.eth.abi.decodeParameter('uint256', res.value),
+            res.block.number,
+        )
     })
-    assert.equal(res.receipt.status, conf.successStatus)
 
-    // submit a transaction that calls blockTime()
-    let blockTimeData = deployed.contract.methods.blockNumber().encodeABI()
-    res = await helpers.signAndSend({
-        from: conf.eoa.address,
-        to: contractAddress,
-        data: blockTimeData,
-        value: '0',
-        gasPrice: conf.minGasPrice,
+    it('calls blockTime', async function() {
+        await testEmitTx(methods.emitBlockTime())
+
+        let res = await testCall(methods.blockTime())
+        assert.equal(
+            web3.eth.abi.decodeParameter('uint', res.value),
+            res.block.timestamp,
+        )
     })
-    assert.equal(res.receipt.status, conf.successStatus)
 
-    // submit a transaction that calls blockHash(uint num)
-    let blockHashData = deployed.contract.methods.blockHash(110).encodeABI()
-    res = await helpers.signAndSend({
-        from: conf.eoa.address,
-        to: contractAddress,
-        data: blockHashData,
-        value: '0',
-        gasPrice: conf.minGasPrice,
+    it('calls blockHash', async function() {
+        await testEmitTx(methods.emitBlockHash())
+
+        let res = await testCall(methods.blockHash())
+        assert.equal(
+            web3.eth.abi.decodeParameter('bytes32', res.value),
+            res.block.hash,
+        )
     })
-    assert.equal(res.receipt.status, conf.successStatus)
 
-    // submit a transaction that calls random()
-    let randomData = deployed.contract.methods.random().encodeABI()
-    res = await helpers.signAndSend({
-        from: conf.eoa.address,
-        to: contractAddress,
-        data: randomData,
-        value: '0',
-        gasPrice: conf.minGasPrice,
+    it('calls random', async function() {
+        await testEmitTx(methods.emitRandom())
+
+        let res = await testCall(methods.random())
+        assert.equal(
+            web3.eth.abi.decodeParameter('uint256', res.value),
+            res.block.difficulty
+        )
     })
-    assert.equal(res.receipt.status, conf.successStatus)
 
-    // submit a transaction that calls chainID()
-    let chainIDData = deployed.contract.methods.chainID().encodeABI()
-    res = await helpers.signAndSend({
-        from: conf.eoa.address,
-        to: contractAddress,
-        data: chainIDData,
-        value: '0',
-        gasPrice: conf.minGasPrice,
+    it('calls chainID', async function() {
+        await testEmitTx(methods.emitChainID())
+        await testCall(methods.chainID())
     })
-    assert.equal(res.receipt.status, conf.successStatus)
 
-    // submit a transaction that calls verifyArchCallToRandomSource(uint64 height)
-    let getRandomSourceData = deployed.contract.methods.verifyArchCallToRandomSource(120).encodeABI()
-    res = await helpers.signAndSend({
-        from: conf.eoa.address,
-        to: contractAddress,
-        data: getRandomSourceData,
-        value: '0',
-        gasPrice: conf.minGasPrice,
+    it('calls verifyArchCallToFlowBlockHeight', async function() {
+        await testEmitTx(methods.emitVerifyArchCallToFlowBlockHeight())
+
+        let res = await testCall(methods.verifyArchCallToFlowBlockHeight())
+        assert.equal(
+            web3.eth.abi.decodeParameter('uint64', res.value),
+            res.block.number,
+        )
     })
-    assert.equal(res.receipt.status, conf.successStatus)
 
-    /*
-    * // make a contract call for verifyArchCallToRandomSource(uint64 height)
-    res = await web3.eth.call({ to: contractAddress, data: getRandomSourceData }, latest)
-    assert.notEqual(
-        res,
-        '0x0000000000000000000000000000000000000000000000000000000000000000'
-    )
-    assert.lengthOf(res, 66)
+    it('calls verifyArchCallToRandomSource', async function() {
+        await testEmitTx(methods.emitVerifyArchCallToRandomSource(1))
 
-    // submit a transaction that calls verifyArchCallToRevertibleRandom()
-    let revertibleRandomData = deployed.contract.methods.verifyArchCallToRevertibleRandom().encodeABI()
-    res = await helpers.signAndSend({
-        from: conf.eoa.address,
-        to: contractAddress,
-        data: revertibleRandomData,
-        value: '0',
-        gasPrice: conf.minGasPrice,
+        let res = await testCall(methods.verifyArchCallToRandomSource(1))
+        assert.notEqual(
+            res.value,
+            '0x0000000000000000000000000000000000000000000000000000000000000000'
+        )
+        assert.lengthOf(res.value, 66)
     })
-    assert.equal(res.receipt.status, conf.successStatus)
 
-    // make a contract call for verifyArchCallToRevertibleRandom()
-    res = await web3.eth.call({ to: contractAddress, data: revertibleRandomData }, latest)
-    assert.notEqual(
-        res,
-        '0x0000000000000000000000000000000000000000000000000000000000000000'
-    )
-    assert.lengthOf(res, 66)
 
-    // submit a transaction that calls verifyArchCallToFlowBlockHeight()
-    let flowBlockHeightData = deployed.contract.methods.verifyArchCallToFlowBlockHeight().encodeABI()
-    res = await helpers.signAndSend({
-        from: conf.eoa.address,
-        to: contractAddress,
-        data: flowBlockHeightData,
-        value: '0',
-        gasPrice: conf.minGasPrice,
+    it('calls verifyArchCallToRevertibleRandom', async function() {
+        await testEmitTx(methods.emitVerifyArchCallToRevertibleRandom())
+
+        let res = await testCall(methods.verifyArchCallToRevertibleRandom())
+        assert.notEqual(
+            res.value,
+            '0x0000000000000000000000000000000000000000000000000000000000000000'
+        )
+        assert.lengthOf(res.value, 66)
     })
-    assert.equal(res.receipt.status, conf.successStatus)
 
-    // make a contract call for verifyArchCallToFlowBlockHeight()
-    res = await web3.eth.call({ to: contractAddress, data: flowBlockHeightData }, latest)
-    assert.equal(
-        web3.eth.abi.decodeParameter('uint64', res),
-        latest,
-    )
+    it('calls verifyArchCallToVerifyCOAOwnershipProof', async function() {
+        let tx = await web3.eth.getTransactionFromBlock(conf.startBlockHeight, 1)
+        let bytes = web3.utils.hexToBytes('f853c18088f8d6e0586b0a20c78365766df842b840b90448f4591df2639873be2914c5560149318b7e2fcf160f7bb8ed13cfd97be2f54e6889606f18e50b2c37308386f840e03a9fff915f57b2164cba27f0206a95')
+        let addr = '0x1bacdb569847f31ade07e83d6bb7cefba2b9290b35d5c2964663215e73519cff'
 
-    // submit a transaction that calls verifyArchCallToVerifyCOAOwnershipProof(address,bytes32,bytes)
-    let tx = await web3.eth.getTransactionFromBlock(conf.startBlockHeight, 1)
-    let verifyCOAOwnershipProofData = deployed.contract.methods.verifyArchCallToVerifyCOAOwnershipProof(
-        tx.to,
-        '0x1bacdb569847f31ade07e83d6bb7cefba2b9290b35d5c2964663215e73519cff',
-        web3.utils.hexToBytes('f853c18088f8d6e0586b0a20c78365766df842b840b90448f4591df2639873be2914c5560149318b7e2fcf160f7bb8ed13cfd97be2f54e6889606f18e50b2c37308386f840e03a9fff915f57b2164cba27f0206a95')
-    ).encodeABI()
-    res = await helpers.signAndSend({
-        from: conf.eoa.address,
-        to: contractAddress,
-        data: verifyCOAOwnershipProofData,
-        value: '0',
-        gasPrice: conf.minGasPrice,
+        await testEmitTx(methods.emitVerifyArchCallToVerifyCOAOwnershipProof(tx.to, addr, bytes))
+
+        let res = await testCall(methods.verifyArchCallToVerifyCOAOwnershipProof(tx.to, addr, bytes))
+        assert.equal(
+            web3.eth.abi.decodeParameter('bool', res.value),
+            false,
+        )
     })
-    assert.equal(res.receipt.status, conf.successStatus)
-
-    // make a contract call for verifyArchCallToVerifyCOAOwnershipProof(address,bytes32,bytes)
-    res = await web3.eth.call({ to: contractAddress, data: verifyCOAOwnershipProofData }, latest)
-    assert.equal(
-        web3.eth.abi.decodeParameter('bool', res),
-        false,
-    )
-    * */
 })
