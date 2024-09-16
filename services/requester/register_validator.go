1d9e57fa986eda3c925902eb7a0272f106ac69f4
sideninja <75445744+sideninja@users.noreply.github.com>
2024-09-16 15:41:35 +0200

    add typed error and comments

--- services/requester/register_validator.go
+++ services/requester/register_validator.go
@@ -12,6 +12,7 @@ import (
 	"github.com/onflow/flow/protobuf/go/flow/executiondata"
 	"golang.org/x/exp/maps"
 
+	errs "github.com/onflow/flow-evm-gateway/models/errors"
 	"github.com/onflow/flow-evm-gateway/storage/pebble"
 )
 
@@ -20,10 +21,23 @@ import (
 
 var _ atree.Ledger = &RegisterValidator{}
 
+// RegisterValidator keeps track of all set register during execution and is checked
+// once the block is executed.
 type RegisterValidator struct {
 	*pebble.Register
 	execution executiondata.ExecutionDataAPIClient
 	updates   map[flow.RegisterID][]byte
+}
+
+func NewRegisterValidator(
+	register *pebble.Register,
+	execution executiondata.ExecutionDataAPIClient,
+) *RegisterValidator {
+	return &RegisterValidator{
+		Register:  register,
+		execution: execution,
+		updates:   make(map[flow.RegisterID][]byte),
+	}
 }
 
 func (r *RegisterValidator) SetValue(owner, key, value []byte) (err error) {
@@ -36,6 +50,11 @@ func (r *RegisterValidator) SetValue(owner, key, value []byte) (err error) {
 	return r.Register.SetValue(owner, key, value)
 }
 
+// ValidateBlock will go over all registers that were set during block execution and compare
+// them against the registers stored on-chain using an execution data client.
+// Expected errors:
+// - Invalid error if there is a mismatch in any of the register values
+// Any other error is an issue with client request or response.
 func (r *RegisterValidator) ValidateBlock(height uint64) error {
 	registers := make([]*entities.RegisterID, 0)
 	values := maps.Values(r.updates)
@@ -58,7 +77,8 @@ func (r *RegisterValidator) ValidateBlock(height uint64) error {
 	for i, val := range response.Values {
 		if !bytes.Equal(values[i], val) {
 			return fmt.Errorf(
-				"register %s with value %x does not match remote state value %x at height %d",
+				"%w register %s with value %x does not match remote state value %x at height %d",
+				errs.ErrInvalid,
 				maps.Keys(r.updates)[i].String(),
 				values[i],
 				val,
