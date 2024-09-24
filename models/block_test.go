f94f3ecb9442cb8259e1fc543f57019e558b3a28
Ardit Marku <markoupetr@gmail.com>
2024-09-24 12:16:50 +0300

    Update logic for decoding legacy EVM Block events

--- models/block_test.go
+++ models/block_test.go
@@ -150,7 +150,7 @@ func Test_DecodingLegacyBlockExecutedEvent(t *testing.T) {
 		hashToCadenceArrayValue(block.TransactionHashRoot),
 	}).WithType(eventType)
 
-	b, err := decodeLegacyBlockEvent(legacyEvent)
+	b, err := decodeBlockEvent(legacyEvent)
 	require.NoError(t, err)
 
 	require.Equal(t, block.ParentBlockHash, b.ParentBlockHash)
@@ -160,10 +160,7 @@ func Test_DecodingLegacyBlockExecutedEvent(t *testing.T) {
 	dech, err := b.Hash()
 	require.NoError(t, err)
 	require.Equal(t, bh, dech)
-
-	b2, err := decodeBlockEvent(legacyEvent)
-	require.NoError(t, err)
-	require.Equal(t, b, b2)
+	require.NotNil(t, b.FixedHash)
 }
 
 func Test_Hash(t *testing.T) {
