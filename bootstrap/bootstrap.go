0056128296a16b541885f1e17939af038a2787ec
Ardit Marku <markoupetr@gmail.com>
2024-09-30 09:40:03 +0300

    Add the EVM GW version to log lines

--- bootstrap/bootstrap.go
+++ bootstrap/bootstrap.go
@@ -57,7 +57,8 @@ type Bootstrap struct {
 
 func New(config *config.Config) (*Bootstrap, error) {
 	logger := zerolog.New(config.LogWriter).
-		With().Timestamp().Logger().Level(config.LogLevel)
+		With().Timestamp().Str("version", api.Version).
+		Logger().Level(config.LogLevel)
 
 	client, err := setupCrossSporkClient(config, logger)
 	if err != nil {
