d61a01b27cca7437a1df5747267798789f197ec6
Ardit Marku <markoupetr@gmail.com>
2024-10-01 13:11:33 +0300

    Move CrossSporkClient to evm package

--- bootstrap/bootstrap.go
+++ bootstrap/bootstrap.go
@@ -20,6 +20,7 @@ import (
 	"github.com/onflow/flow-evm-gateway/metrics"
 	"github.com/onflow/flow-evm-gateway/models"
 	errs "github.com/onflow/flow-evm-gateway/models/errors"
+	"github.com/onflow/flow-evm-gateway/services/evm"
 	"github.com/onflow/flow-evm-gateway/services/ingestion"
 	"github.com/onflow/flow-evm-gateway/services/requester"
 	"github.com/onflow/flow-evm-gateway/services/traces"
@@ -45,7 +46,7 @@ type Publishers struct {
 type Bootstrap struct {
 	logger     zerolog.Logger
 	config     *config.Config
-	client     *requester.CrossSporkClient
+	client     *evm.CrossSporkClient
 	storages   *Storages
 	publishers *Publishers
 	collector  metrics.Collector
@@ -402,7 +403,7 @@ func StartEngine(
 }
 
 // setupCrossSporkClient sets up a cross-spork AN client.
-func setupCrossSporkClient(config *config.Config, logger zerolog.Logger) (*requester.CrossSporkClient, error) {
+func setupCrossSporkClient(config *config.Config, logger zerolog.Logger) (*evm.CrossSporkClient, error) {
 	// create access client with cross-spork capabilities
 	currentSporkClient, err := grpc.NewClient(
 		config.AccessNodeHost,
@@ -428,7 +429,7 @@ func setupCrossSporkClient(config *config.Config, logger zerolog.Logger) (*reque
 	}
 
 	// initialize cross spork client to the access nodes
-	client, err := requester.NewCrossSporkClient(
+	client, err := evm.NewCrossSporkClient(
 		currentSporkClient,
 		pastSporkClients,
 		logger,
@@ -445,7 +446,7 @@ func setupCrossSporkClient(config *config.Config, logger zerolog.Logger) (*reque
 // in case such a height doesn't already exist in the database.
 func setupStorage(
 	config *config.Config,
-	client *requester.CrossSporkClient,
+	client *evm.CrossSporkClient,
 	logger zerolog.Logger,
 ) (*Storages, error) {
 	// create pebble storage from the provided database root directory
