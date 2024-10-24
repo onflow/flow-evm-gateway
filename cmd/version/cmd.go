34dfa3015ad66ceb02411a57e485cf5840bdc2ba
Ardit Marku <markoupetr@gmail.com>
2024-10-24 12:43:05 +0300

    Output version info from version command to output channel

--- cmd/version/cmd.go
+++ cmd/version/cmd.go
@@ -1,6 +1,8 @@
 package version
 
 import (
+	"os"
+
 	"github.com/onflow/flow-evm-gateway/api"
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
@@ -10,6 +12,7 @@ var Cmd = &cobra.Command{
 	Use:   "version",
 	Short: "Prints the current version of the EVM Gateway Node",
 	Run: func(*cobra.Command, []string) {
-		log.Info().Str("version", api.Version).Msg("build details")
+		logger := log.Output(os.Stdout)
+		logger.Info().Str("version", api.Version).Msg("build details")
 	},
 }
