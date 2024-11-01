a21b46058392d8d4c5f35fd3ae5ea686bc320078
Janez Podhostnik <janez.podhostnik@gmail.com>
2024-11-01 15:20:09 +0100

    register store add tests

--- storage/pebble/register_test.go
+++ storage/pebble/register_test.go
@@ -3,6 +3,8 @@ package pebble
 import (
 	"testing"
 
+	"github.com/cockroachdb/pebble"
+
 	flowGo "github.com/onflow/flow-go/model/flow"
 
 	"github.com/stretchr/testify/require"
@@ -152,5 +154,54 @@ func Test_Register(t *testing.T) {
 
 		err := r.SetValue(owner2, key, value1)
 		require.Error(t, err)
+	})
+
+	runDB("non-indexed batch", t, func(t *testing.T, db *Storage) {
+		t.Parallel()
+
+		batch := db.db.NewBatch()
+
+		r := NewRegister(db, 0, flowGo.BytesToAddress(owner), batch)
+
+		err := r.SetValue(owner, key, value1)
+		require.NoError(t, err)
+
+		// non-indexed batch will panic on set
+
+		require.Panics(t, func() {
+			_, _ = r.GetValue(owner, key)
+		})
+	})
+
+	runDB("indexed batch", t, func(t *testing.T, db *Storage) {
+		t.Parallel()
+
+		// note that the Storage already creates an indexed batch
+		batch := db.NewBatch()
+
+		r := NewRegister(db, 0, flowGo.BytesToAddress(owner), batch)
+
+		err := r.SetValue(owner, key, value1)
+		require.NoError(t, err)
+
+		// indexed batch will not panic on set
+		v, err := r.GetValue(owner, key)
+		require.NoError(t, err)
+		require.Equal(t, value1, v)
+
+		// outside of the batch the value is still empty
+		r = NewRegister(db, 0, flowGo.BytesToAddress(owner), nil)
+		v, err = r.GetValue(owner, key)
+		require.NoError(t, err)
+		require.Len(t, v, 0)
+
+		// commit the batch
+		err = batch.Commit(pebble.Sync)
+		require.NoError(t, err)
+
+		// now the value is set
+		v, err = r.GetValue(owner, key)
+		require.NoError(t, err)
+		require.Equal(t, value1, v)
 	})
 }
