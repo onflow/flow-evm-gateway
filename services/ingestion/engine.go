236adfd6777f9952fdef6032105933f6d09a8c5b
Ardit Marku <markoupetr@gmail.com>
2024-11-17 12:19:41 +0200

    Handle error returned from state.NewBaseView creation

--- services/ingestion/engine.go
+++ services/ingestion/engine.go
@@ -261,7 +261,10 @@ func (e *Engine) indexEvents(events *models.CadenceEvents, batch *pebbleDB.Batch
 		state := evmStorage.NewEphemeralStorage(evmStorage.NewReadOnlyStorage(st))
 
 		//delete 0 address to fix gasUsage issue on testnet
-		base, _ := evmState.NewBaseView(state, e.replayerConfig.RootAddr)
+		base, err := evmState.NewBaseView(state, e.replayerConfig.RootAddr)
+		if err != nil {
+			return err
+		}
 		base.DeleteAccount(gethCommon.Address{})
 		base.Commit()
 	}
