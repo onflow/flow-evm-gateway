1f74dd818223d516f8026090f357bf44e0df36df
Leo Zhang (zhangchiqing) <zhangchiqing@gmail.com>
2024-11-27 13:34:17 -0800

    get account balance

--- cmd/util/main_test.go
+++ cmd/util/main_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	"github.com/onflow/flow-evm-gateway/storage/pebble"
+	gethCommon "github.com/onflow/go-ethereum/common"
 
 	"github.com/onflow/flow-go/fvm/evm"
 	"github.com/onflow/flow-go/fvm/evm/emulator/state"
@@ -18,11 +19,16 @@ import (
 
 func TestStateDiff(t *testing.T) {
 
-	state1 := ExtractEVMState(t, flowGo.Testnet,
-		"/var/flow/gw/data/db", uint64(218215348))
-	state2 := EVMStateFromReplayGobDir(t,
-		"/var/flow2/evm-state-from-gobs-218215348/", uint64(218215348))
-	// state2 := state.EVMStateFromCheckpointExtract(t, "/var/flow2/evm-state-from-checkpoint-218215348/")
+	height := uint64(218215348)
+
+	// state1 := ExtractEVMState(t, flowGo.Testnet, "/var/flow52/evm/data/db", height)
+
+	state1 := EVMStateFromReplayGobDir(t, "/var/flow/nov26_testnet_evm_state_gob", height)
+	state2 := EVMStateFromCheckpointExtract(t, "/var/flow52/evm-state-from-checkpoint-218215348-migrated/")
+
+	// state1 := ExtractEVMState(t, flowGo.Testnet,
+	//      "/var/flow52/evm/data/db", uint64(228901661))
+	// state2 := EVMStateFromCheckpointExtract(t, "/var/flow52/evm-state-from-checkpoint-228901661")
 
 	differences := state.Diff(state1, state2)
 
@@ -55,7 +61,7 @@ func ExtractEVMState(
 }
 
 func EVMStateFromCheckpointExtract(t *testing.T, dir string) *state.EVMState {
-	enState, err := state.ImportEVMStateFromGob("/var/flow2/evm-state-from-gobs-218215348/")
+	enState, err := state.ImportEVMStateFromGob(dir)
 	require.NoError(t, err)
 	return enState
 }
@@ -89,8 +95,8 @@ func evmStateGobFileNamesByEndHeight(evmStateGobDir string, endHeight uint64) (s
 
 func TestReadPebbleRootAccountStorageKeyID(t *testing.T) {
 	chainID := flowGo.Testnet
-	registerStoreDir := "/var/flow/gw/data/db"
-	flowHeight := uint64(218215348)
+	registerStoreDir := "/var/flow52/evm/data/db"
+	flowHeight := uint64(228901661)
 
 	store, err := pebble.New(registerStoreDir, log.Logger)
 	require.NoError(t, err)
@@ -109,4 +115,31 @@ func TestReadPebbleRootAccountStorageKeyID(t *testing.T) {
 
 	_, err = registerStore.GetSnapshotAt(flowHeight)
 	require.NoError(t, err)
+}
+
+func TestReadAccount(t *testing.T) {
+
+	chainID := flowGo.Testnet
+	registerStoreDir := "/var/flow52/evm/data/db"
+	flowHeight := uint64(228901661)
+	accountStr := "17DDB8C5aA1382Fb04d057e3ec42B65a43A5fF49"
+
+	// Convert string to common.Address
+	account := gethCommon.HexToAddress(accountStr)
+
+	store, err := pebble.New(registerStoreDir, log.Logger)
+	require.NoError(t, err)
+
+	storageRoot := evm.StorageAccountAddress(chainID)
+	registerStore := pebble.NewRegisterStorage(store, storageRoot)
+
+	snapshot, err := registerStore.GetSnapshotAt(flowHeight)
+	require.NoError(t, err)
+	ledger := storage.NewEphemeralStorage(storage.NewReadOnlyStorage(snapshot))
+
+	stateDB, err := state.NewStateDB(ledger, storageRoot)
+	require.NoError(t, err)
+
+	balance := stateDB.GetBalance(account)
+	fmt.Println("balance: ", balance)
 }
