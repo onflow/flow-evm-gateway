cf5aaf2d74396a1ef9e5ad502828a03963aae361
Ardit Marku <markoupetr@gmail.com>
2025-01-08 16:59:33 +0200

    Update API of requester.BlocksProvider type to avoid concurrency issues

--- bootstrap/bootstrap.go
+++ bootstrap/bootstrap.go
@@ -208,11 +208,6 @@ func (b *Bootstrap) StartAPIServer(ctx context.Context) error {
 		b.config,
 	)
 
-	blocksProvider := requester.NewBlocksProvider(
-		b.storages.Blocks,
-		b.config.FlowNetworkID,
-	)
-
 	accountKeys := make([]*requester.AccountKey, 0)
 	if !b.config.IndexOnly {
 		account, err := b.client.GetAccount(ctx, b.config.COAAddress)
@@ -245,7 +240,6 @@ func (b *Bootstrap) StartAPIServer(ctx context.Context) error {
 
 	evm, err := requester.NewEVM(
 		b.storages.Registers,
-		blocksProvider,
 		b.client,
 		b.config,
 		b.logger,
