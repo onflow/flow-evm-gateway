8e7ddc46d44031a289a27217fb4a62a2dfba6255
Ardit Marku <markoupetr@gmail.com>
2025-02-20 13:00:45 +0200

    Add check to avoid underflow in GetSnapshotAt

--- storage/pebble/register_storage.go
+++ storage/pebble/register_storage.go
@@ -181,9 +181,15 @@ func newLookupKey(height uint64, key []byte) *lookupKey {
 // GetSnapshotAt returns a snapshot of the register index at the start of the
 // given block height (which is the end of the previous block).
 // The snapshot has a cache. Nil values are cached.
-func (r *RegisterStorage) GetSnapshotAt(evmBlockHeight uint64) (types.BackendStorageSnapshot, error) {
-	// `evmBlockHeight-1` to get the end state of the previous block.
-	return NewStorageSnapshot(r.Get, evmBlockHeight-1), nil
+func (r *RegisterStorage) GetSnapshotAt(
+	evmBlockHeightOfStartStateToQuery uint64,
+) (types.BackendStorageSnapshot, error) {
+	// Avoid a possible underflow
+	if evmBlockHeightOfStartStateToQuery > 0 {
+		// `evmBlockHeightOfStartStateToQuery-1` to get the end state of the previous block.
+		evmBlockHeightOfStartStateToQuery -= 1
+	}
+	return NewStorageSnapshot(r.Get, evmBlockHeightOfStartStateToQuery), nil
 }
 
 func registerOwnerMismatch(expected flow.Address, owner flow.Address) error {
