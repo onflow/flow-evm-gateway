85d8fbdc53c2968bb0148df10ed6431c76e7f0df
Ardit Marku <markoupetr@gmail.com>
2025-08-25 12:31:06 +0300

    Move gas price calculation to FeeParameters

--- models/fee_parameters.go
+++ models/fee_parameters.go
@@ -2,15 +2,20 @@ package models
 
 import (
 	"fmt"
+	"math/big"
 
 	"github.com/ethereum/go-ethereum/rlp"
 	"github.com/onflow/cadence"
 )
 
+const feeParamsPrecision = 100_000_000
+
+var surgeFactorScale = big.NewInt(feeParamsPrecision)
+
 var DefaultFeeParameters = &FeeParameters{
-	SurgeFactor:         cadence.UFix64(100_000_000),
-	InclusionEffortCost: cadence.UFix64(100_000_000),
-	ExecutionEffortCost: cadence.UFix64(100_000_000),
+	SurgeFactor:         cadence.UFix64(feeParamsPrecision),
+	InclusionEffortCost: cadence.UFix64(feeParamsPrecision),
+	ExecutionEffortCost: cadence.UFix64(feeParamsPrecision),
 }
 
 type FeeParameters struct {
@@ -21,6 +26,11 @@ type FeeParameters struct {
 
 func (f *FeeParameters) ToBytes() ([]byte, error) {
 	return rlp.EncodeToBytes(f)
+}
+
+func (f *FeeParameters) CalculateGasPrice(currentGasPrice *big.Int) *big.Int {
+	gasPrice := new(big.Int).SetUint64(currentGasPrice.Uint64() * uint64(f.SurgeFactor))
+	return new(big.Int).Div(gasPrice, surgeFactorScale)
 }
 
 func NewFeeParametersFromBytes(data []byte) (*FeeParameters, error) {
